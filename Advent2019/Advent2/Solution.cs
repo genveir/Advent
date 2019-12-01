f18692f3eea3f1ca017777b125a475ad9f824713
Geerten Vink <g.j.a.vink@students.uu.nl>
2019-12-01 15:05:15 +0100

    prep voor dag 2

---
+++ Advent2019/Advent2/Solution.cs
@@ -0,0 +1,47 @@
+﻿using Advent2019.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2019.Advent2
+{
+    public class Solution : ISolution
+    {
+        IEnumerable<ParsedInput> parsedInput;
+
+        public Solution(Input.InputMode inputMode, string input)
+        {
+            var lines = Input.GetInputLines(inputMode, input).ToArray();
+
+            parsedInput = ParsedInput.Parse(lines);
+        }
+        public Solution() : this(Input.InputMode.Embedded, "Input") { }
+
+        private class ParsedInput
+        {
+
+            public static IEnumerable<ParsedInput> Parse(IEnumerable<string> lines)
+            {
+                var parsedInputs = new List<ParsedInput>();
+
+                foreach(var line in lines)
+                {
+
+                }
+
+                return parsedInputs;
+            }
+        }
+
+        public string GetResult1()
+        {
+            return "";
+        }
+
+        public string GetResult2()
+        {
+            return "";
+        }
+    }
+}
