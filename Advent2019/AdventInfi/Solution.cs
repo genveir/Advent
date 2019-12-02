eac9666014db5450f53f69a75d89a56dee931f3b
Geerten Vink <g.j.a.vink@students.uu.nl>
2019-12-02 05:52:51 +0100

    Infi part 1

---
+++ Advent2019/AdventInfi/Solution.cs
@@ -0,0 +1,91 @@
+﻿using Advent2019.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2019.AdventInfi
+{
+    public class Solution : ISolution
+    {
+        static (int X, int Y) kerstman;
+
+        Dictionary<int, int> flats;
+        List<Sprong> sprongen;
+
+        public Solution(Input.InputMode inputMode, string input)
+        {
+            var lines = Input.GetInputLines(inputMode, input, new char[] { ' ', ':', 's', '}', '{', '\\', '\n', '\r', '\"' }).ToArray();
+
+            flats = Flat.Parse(lines[1].Split(new char[] { ',', ']', '[' }, StringSplitOptions.RemoveEmptyEntries));
+            sprongen = Sprong.Parse(lines[3].Split(new char[] { ',', ']', '[' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+        public Solution() : this(Input.InputMode.Embedded, "Input") { }
+
+        private class Flat
+        {
+            public static Dictionary<int, int> Parse(string[] lines)
+            {
+                var flats = new Dictionary<int, int>();
+
+                for (int n = 0; n < lines.Length; n += 2)
+                {
+                    int X = int.Parse(lines[n]);
+                    int Y = int.Parse(lines[n + 1]);
+
+                    if (n == 0) kerstman = (X, Y);
+
+                    flats.Add(X, Y);
+                }
+
+                return flats;
+            }
+        }
+
+        private class Sprong
+        {
+            public int X;
+            public int Y;
+
+            public static List<Sprong> Parse(string[] lines)
+            {
+                var sprongen = new List<Sprong>();
+
+                for (int n = 0; n < lines.Length; n += 2)
+                {
+                    var s = new Sprong()
+                    {
+                        X = int.Parse(lines[n]),
+                        Y = int.Parse(lines[n + 1])
+                    };
+
+                    sprongen.Add(s);
+                }
+
+                return sprongen;
+            }
+        }
+
+        public string GetResult1()
+        {
+            for (int n = 0; n < sprongen.Count; n++)
+            {
+                kerstman.X++;
+                kerstman.X += sprongen[n].X;
+                kerstman.Y += sprongen[n].Y;
+
+                if (flats.ContainsKey(kerstman.X) && flats[kerstman.X] <= kerstman.Y) kerstman.Y = flats[kerstman.X];
+                else return (n + 1).ToString();
+            }
+
+            return "0";
+        }
+
+        public string GetResult2()
+        {
+
+
+            return "";
+        }
+    }
+}
---
+++ Advent2019/AdventInfi/Tests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent2019.AdventInfi
+{ 
+    class Tests
+    {
+        [TestCase("{\"flats\": [[1,4],[3,8],[4,3],[5,7],[7,4],[10,3]],\"sprongen\": [[2,0],[0,4],[1,0],[0,0]]}", "4")]
+        public void Test1(string input, string output)
+        {
+            var sol = new Solution(Shared.Input.InputMode.String, input);
+
+            Assert.AreEqual(output, sol.GetResult1());
+        }
+
+        [TestCase("", "")]
+        public void Test2(string input, string output)
+        {
+            var sol = new Solution(Shared.Input.InputMode.String, input);
+
+            Assert.AreEqual(output, sol.GetResult2());
+        }
+    }
+}
