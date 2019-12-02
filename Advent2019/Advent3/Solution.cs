1e4027b5c307ce5f1e68068f8ea6cce741ceb8f0
Geerten Vink <g.j.a.vink@students.uu.nl>
2019-12-02 22:48:21 +0100

    Advent3 prep

---
+++ Advent2019/Advent3/Solution.cs
@@ -0,0 +1,52 @@
+﻿using Advent2019.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2019.Advent3
+{
+    public class Solution : ISolution
+    {
+        IEnumerable<ParsedInput> modules;
+
+        public Solution(Input.InputMode inputMode, string input)
+        {
+            var lines = Input.GetInputLines(inputMode, input).ToArray();
+
+            modules = ParsedInput.Parse(lines);
+        }
+        public Solution() : this(Input.InputMode.Embedded, "Input") { }
+
+        private class ParsedInput
+        {
+
+            public static IEnumerable<ParsedInput> Parse(IEnumerable<string> lines)
+            {
+                var parsedInputs = new List<ParsedInput>();
+
+                foreach(var line in lines)
+                {
+                    var pi = new ParsedInput()
+                    {
+
+                    };
+
+                    parsedInputs.Add(pi);
+                }
+
+                return parsedInputs;
+            }
+        }
+
+        public string GetResult1()
+        {
+            return "";
+        }
+
+        public string GetResult2()
+        {
+            return "";
+        }
+    }
+}
