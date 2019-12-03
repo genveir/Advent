76483053b97b07e4ccd80a3b2e0ebe960edccf35
Geerten Vink <g.j.a.vink@students.uu.nl>
2019-12-03 06:24:26 +0100

    Advent3 feitelijke oplossing

--- Advent2019/Advent3/Solution.cs
+++ Advent2019/Advent3/Solution.cs
@@ -8,45 +8,88 @@ namespace Advent2019.Advent3
 {
     public class Solution : ISolution
     {
-        IEnumerable<ParsedInput> modules;
+        static Dictionary<(int x, int y), int> steps;
+        HashSet<(int x, int y)>[] wires;
 
         public Solution(Input.InputMode inputMode, string input)
         {
             var lines = Input.GetInputLines(inputMode, input).ToArray();
 
-            modules = ParsedInput.Parse(lines);
+            wires = ParsedInput.Parse(lines);
+            
         }
         public Solution() : this(Input.InputMode.Embedded, "Input") { }
 
         private class ParsedInput
         {
-
-            public static IEnumerable<ParsedInput> Parse(IEnumerable<string> lines)
+            public static HashSet<(int x, int y)>[] Parse(IEnumerable<string> lines)
             {
-                var parsedInputs = new List<ParsedInput>();
+                steps = new Dictionary<(int x, int y), int>();
 
+                var result = new HashSet<(int x, int y)>[2];
+                int num = 0;
                 foreach(var line in lines)
                 {
-                    var pi = new ParsedInput()
+                    var split = line.Split(",", StringSplitOptions.RemoveEmptyEntries);
+                    split = split.Select(s => s.Trim()).ToArray();
+                    HashSet<(int x, int y)> wire = new HashSet<(int x, int y)>();
+                    int x = 0;
+                    int y = 0;
+                    int totalDist = 0;
+                    foreach(var route in split)
                     {
+                        var direction = route[0];
+                        var distance = int.Parse(route.Substring(1));
 
-                    };
+                        for(int n = 0; n < distance; n++)
+                        {
+                            switch(direction)
+                            {
+                                case 'R': x++; wire.Add((x, y)); break;
+                                case 'L': x--; wire.Add((x, y)); break;
+                                case 'U': y++; wire.Add((x, y)); break;
+                                case 'D': y--; wire.Add((x, y)); break;
+                            }
+                            totalDist++;
 
-                    parsedInputs.Add(pi);
+                            if (!steps.ContainsKey((x, y))) steps[(x, y)] = totalDist;
+                            else steps[(x, y)] = steps[(x, y)] + totalDist;
+                        }
+                    }
+
+                    result[num++] = wire;
                 }
 
-                return parsedInputs;
+                return result;
             }
         }
 
         public string GetResult1()
         {
-            return "";
+            var intersect = wires[0].Intersect(wires[1]);
+
+            int minDist = int.MaxValue;
+            foreach(var point in intersect)
+            {
+                var dist = Math.Abs(point.x) + Math.Abs(point.y);
+                if (dist < minDist) minDist = dist;
+            }
+
+            return minDist.ToString();
         }
 
         public string GetResult2()
         {
-            return "";
+            var intersect = wires[0].Intersect(wires[1]);
+
+            int minDist = int.MaxValue;
+            foreach (var point in intersect)
+            {
+                var dist = steps[(point.x, point.y)];
+                if (dist < minDist) minDist = dist;
+            }
+
+            return minDist.ToString();
         }
     }
 }
--- Advent2019/Advent3/Tests.cs
+++ Advent2019/Advent3/Tests.cs
@@ -7,7 +7,8 @@ namespace Advent2019.Advent3
 {
     class Tests
     {
-        [TestCase("", "")]
+        [TestCase("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62, R66, U55, R34, D71, R55, D58, R83", "159")]
+        [TestCase("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98, R91, D20, R16, D67, R40, U7, R15, U6, R7", "135")]
         public void Test1(string input, string output)
         {
             var sol = new Solution(Shared.Input.InputMode.String, input);
@@ -15,7 +16,8 @@ namespace Advent2019.Advent3
             Assert.AreEqual(output, sol.GetResult1());
         }
 
-        [TestCase("", "")]
+        [TestCase("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62, R66, U55, R34, D71, R55, D58, R83", "610")]
+        [TestCase("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98, R91, D20, R16, D67, R40, U7, R15, U6, R7", "410")]
         public void Test2(string input, string output)
         {
             var sol = new Solution(Shared.Input.InputMode.String, input);
