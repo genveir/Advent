9e3b5e8f2bff3971db88cb6bc7d2d566d6b5c8b6
Geerten Vink <maedaros@yahoo.com>
2019-12-10 07:14:28 +0100

    day 10

--- Advent2019/Advent10/Solution.cs
+++ Advent2019/Advent10/Solution.cs
@@ -8,45 +8,150 @@ namespace Advent2019.Advent10
 {
     public class Solution : ISolution
     {
-        IEnumerable<ParsedInput> modules;
+        public List<Asteroid> asteroids;
+        public Dictionary<(int x, int y), Asteroid> locations;
 
         public Solution(Input.InputMode inputMode, string input)
         {
             var lines = Input.GetInputLines(inputMode, input).ToArray();
 
-            modules = ParsedInput.Parse(lines);
+            asteroids = Asteroid.Parse(lines);
+            locations = new Dictionary<(int x, int y), Asteroid>();
+            foreach (var asteroid in asteroids) locations.Add((asteroid.X, asteroid.Y), asteroid);
+
+            FindStation();
+            Find200th();
         }
         public Solution() : this(Input.InputMode.Embedded, "Input") { }
 
-        private class ParsedInput
+        public class Asteroid
         {
+            public int X;
+            public int Y;
+            public List<Asteroid> Visible = new List<Asteroid>();
+            public double AngleFromStation;
 
-            public static IEnumerable<ParsedInput> Parse(IEnumerable<string> lines)
+            public static List<Asteroid> Parse(string[] lines)
             {
-                var parsedInputs = new List<ParsedInput>();
+                var asteroids = new List<Asteroid>();
 
-                foreach(var line in lines)
+                for (int y = 0; y < lines.Count(); y++)
                 {
-                    var pi = new ParsedInput()
+                    var line = lines[y];
+                    for (int x = 0; x < line.Length; x++)
                     {
-
-                    };
-
-                    parsedInputs.Add(pi);
+                        if (line[x] == '.') continue;
+                        else
+                        {
+                            asteroids.Add(new Asteroid() { X = x, Y = y });
+                        }
+                    }
                 }
 
-                return parsedInputs;
+                return asteroids;
             }
+
+            public override string ToString()
+            {
+                return "Asteroid (" + X + ", " + Y + ")";
+            }
+        }
+
+        public int GetGCD(int first, int second)
+        {
+            if (second == 0) return first;
+            if (second > first) return GetGCD(second, first);
+            return GetGCD(first - second, second);
+        }
+
+        public double GetAngle(int X, int Y)
+        {
+            var angle = ((Math.Atan2(Y, X) + 0.5 * Math.PI) + 2.0d * Math.PI) % (2.0d * Math.PI);
+            return angle;
+        }
+
+        public void SetVisible (int asteroidIndex)
+        {
+            var handled = new HashSet<(int, int)>();
+
+            asteroids[asteroidIndex].Visible.Clear();
+
+            for (int secondIndex = 0; secondIndex < asteroids.Count; secondIndex++)
+            {
+                if (asteroidIndex == secondIndex) continue;
+
+                var xShift = asteroids[secondIndex].X - asteroids[asteroidIndex].X;
+                var yShift = asteroids[secondIndex].Y - asteroids[asteroidIndex].Y;
+
+                var GCD = GetGCD(Math.Abs(xShift), Math.Abs(yShift));
+
+                xShift = xShift == 0 ? 0 : xShift / GCD;
+                yShift = yShift == 0 ? 0 : yShift / GCD;
+
+                if (handled.Contains((xShift, yShift))) continue;
+                handled.Add((xShift, yShift));
+
+                int newX = asteroids[asteroidIndex].X + xShift;
+                int newY = asteroids[asteroidIndex].Y + yShift;
+                for (int n = 0; n < 50; n++)
+                {
+                    if (locations.ContainsKey((newX, newY)))
+                    {
+                        asteroids[asteroidIndex].Visible.Add(asteroids[secondIndex]);
+                        break;
+                    }
+
+                    newX += xShift;
+                    newY += yShift;
+                }
+            }
+        }
+
+        public void SetAngles(int asteroidIndex)
+        {
+            var stationAst = asteroids[asteroidIndex];
+
+            foreach (var visible in stationAst.Visible)
+            {
+                visible.AngleFromStation = GetAngle(visible.X - stationAst.X, visible.Y - stationAst.Y);
+            }
+        }
+
+        int highestVisible = 0;
+        int station = -1;
+        public void FindStation()
+        {
+            for (int firstIndex = 0; firstIndex < asteroids.Count; firstIndex++)
+            {
+                SetVisible(firstIndex);
+                if (asteroids[firstIndex].Visible.Count > highestVisible)
+                {
+                    highestVisible = asteroids[firstIndex].Visible.Count;
+                    station = firstIndex;
+                }
+            }
+        }
+
+        Asteroid asteroid200;
+        public void Find200th()
+        {
+            var stationAsteroid = asteroids[station];
+
+            SetAngles(station);
+            var inOrder = stationAsteroid.Visible.OrderBy(ast => ast.AngleFromStation).ToArray();
+
+            if (asteroids.Count > 200)
+                asteroid200 = inOrder[199];
         }
 
         public string GetResult1()
         {
-            return "";
+            return highestVisible.ToString();
         }
 
         public string GetResult2()
         {
-            return "";
+            return (asteroid200.X * 100 + asteroid200.Y).ToString();
         }
     }
 }
--- Advent2019/Advent10/Tests.cs
+++ Advent2019/Advent10/Tests.cs
@@ -7,7 +7,65 @@ namespace Advent2019.Advent10
 {
     class Tests
     {
-        [TestCase("", "")]
+        const string testCase1 = @".#..#
+.....
+#####
+....#
+...##";
+
+        const string testCase2 = @"......#.#.
+#..#.#....
+..#######.
+.#.#.###..
+.#..#.....
+..#....#.#
+#..#....#.
+.##.#..###
+##...#..#.
+.#....####";
+
+        const string testCase3 = @".#..#..###
+####.###.#
+....###.#.
+..###.##.#
+##.##.#.#.
+....###..#
+..#.#..#.#
+#..#.#.###
+.##...##.#
+.....#.#..";
+
+        const string testCase4 = @".#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##";
+
+        const string testCase5 = @".#....#####...#..
+##...##.#####..##
+##...#...#.#####.
+..#.....X...###..
+..#.#.....#....##";
+
+        [TestCase(testCase1, "8")]
+        [TestCase(testCase2, "33")]
+        [TestCase(testCase3, "41")]
+        [TestCase(testCase4, "210")]
         public void Test1(string input, string output)
         {
             var sol = new Solution(Shared.Input.InputMode.String, input);
@@ -15,7 +73,29 @@ namespace Advent2019.Advent10
             Assert.AreEqual(output, sol.GetResult1());
         }
 
-        [TestCase("", "")]
+        [Test]
+        public void GCDTest()
+        {
+            var sol = new Solution();
+
+            Assert.AreEqual(4, sol.GetGCD(100, 4));
+            Assert.AreEqual(4, sol.GetGCD(4, 100));
+            Assert.AreEqual(4, sol.GetGCD(0, 4));
+            Assert.AreEqual(21, sol.GetGCD(1071, 462));
+        }
+
+        [Test]
+        public void AngleTest()
+        {
+            var sol = new Solution();
+
+            Assert.AreEqual(0.0d * Math.PI, sol.GetAngle(0, -1));
+            Assert.AreEqual(0.5d * Math.PI, sol.GetAngle(1, 0));
+            Assert.AreEqual(1.0d * Math.PI, sol.GetAngle(0, 1));
+            Assert.AreEqual(1.5d * Math.PI, sol.GetAngle(-1, 0));
+        }
+
+        [TestCase(testCase4, "802")]
         public void Test2(string input, string output)
         {
             var sol = new Solution(Shared.Input.InputMode.String, input);
