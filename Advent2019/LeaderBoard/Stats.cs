349e1c450e2bac44ec9a3371908249c4195e0875
Geerten Vink <maedaros@yahoo.com>
2019-12-14 12:43:32 +0100

    2nd star leaderboard in stats

--- Advent2019/LeaderBoard/Stats.cs
+++ Advent2019/LeaderBoard/Stats.cs
@@ -10,18 +10,21 @@ namespace Advent2019.LeaderBoard
 {
     public class Stats
     {
+        int lastDay;
+        List<PersonalStats> personalStats;
+
         public Stats()
         {
             var json = Input.GetInput(Input.InputMode.Embedded, "LeaderBoard.txt");
 
             var model = JsonConvert.DeserializeObject<LeaderBoardModel>(json);
 
-            int lastDay = 0;
-            var personalStats = new List<PersonalStats>();
+            lastDay = 0;
+            personalStats = new List<PersonalStats>();
             foreach (var member in model.Members)
             {
                 var memberDayStats = new List<DayStats>();
-                foreach(var cdl in member.Value.completion_day_level)
+                foreach (var cdl in member.Value.completion_day_level)
                 {
                     var dayStats = new DayStats()
                     {
@@ -45,12 +48,17 @@ namespace Advent2019.LeaderBoard
                 });
             }
 
-            ;
+            Print();
+            Print2();
+        }
 
+        public void Print()
+        { 
             for (int n = 1; n <= lastDay; n++)
             {
                 Console.WriteLine("Day " + n);
                 Console.WriteLine("seconds to do part 2:");
+
                 foreach (var person in personalStats)
                 {
                     var dayStat = person.DayStats.SingleOrDefault(ds => ds.Day == n);
@@ -58,12 +66,46 @@ namespace Advent2019.LeaderBoard
                     string timeTaken;
                     if (dayStat?.finish2 == null) timeTaken = "DNS";
                     else if (dayStat.finish2 == 0) timeTaken = "DNF";
-                    else timeTaken = (dayStat.finish2 - dayStat.finish1).ToString();
+                    else timeTaken = dayStat.timeTaken.Value.ToString();
 
                     Console.WriteLine(person.Name.PadRight(20) + timeTaken);
                 }
                 Console.WriteLine();
             }
+        }
+
+        public void Print2()
+        {
+            var stars = new Dictionary<string, long>();
+            foreach (var person in personalStats) stars.Add(person.Name, 0);
+
+            for (int n = 1; n <= lastDay; n++)
+            {
+                var namesAndTimes = new List<(string name, long timeTaken)>();
+                foreach(var person in personalStats)
+                {
+                    var dayStat = person.DayStats.SingleOrDefault(ds => ds.Day == n);
+                    var timeTaken = dayStat?.timeTaken;
+                    if (timeTaken != null)
+                    {
+                        namesAndTimes.Add((person.Name, timeTaken.Value));
+                    }
+                }
+                namesAndTimes = namesAndTimes.OrderBy(nat => nat.timeTaken).ToList();
+
+                int starsYouGet = personalStats.Count;
+                for (int i = 0; i < namesAndTimes.Count; i++)
+                {
+                    var stat = namesAndTimes[i];
+                    stars[stat.name] += starsYouGet;
+                    starsYouGet--;
+                }
+            }
+
+            var asList = stars.OrderByDescending(kv => kv.Value).Select(kv => kv.Key.PadRight(20) + kv.Value).ToList();
+
+            Console.WriteLine("Leaderboard based only on the time between part2 and part1:");
+            foreach (var item in asList) Console.WriteLine(item);
         }
 
 
@@ -84,6 +126,15 @@ namespace Advent2019.LeaderBoard
             
             public long finish1;
             public long finish2;
+
+            public long? timeTaken
+            {
+                get
+                {
+                    if (finish2 == 0) return null;
+                    else return finish2 - finish1;
+                }
+            }
         }
 
 
