ca75f32f65abaaad1293d64e0d3f7ab2022146ae
Geerten Vink <geerten@summit.nl>
2019-12-16 19:07:32 +0100

    just merged the P1 solution back into the P2 solution. Not pretty at all.

--- Advent2019/Advent16/Solution.cs
+++ Advent2019/Advent16/Solution.cs
@@ -24,6 +24,63 @@ namespace Advent2019.Advent16
             program = startProgram.DeepCopy();
         }
 
+        public long[] RunP1Phase()
+        {
+            var newProgram = new long[program.Length];
+
+            for (int positionConsidered = 0; positionConsidered < program.Length; positionConsidered++)
+            {
+                newProgram[positionConsidered] = Calculate(program, positionConsidered);
+            }
+
+            return newProgram;
+        }
+
+        bool Verbose = false;
+
+        public long Calculate(long[] program, long positionConsidered)
+        {
+            long result = 0;
+
+            for (int position = 0; position < program.Length; position++)
+            {
+                var multiplier = GetMultiplierFor(positionConsidered, position);
+
+                if (Verbose) Console.Write(program[position] + "*" + multiplier);
+                if (Verbose && position != program.Length) Console.Write(" + ");
+
+                var val = program[position] * multiplier;
+                result += val;
+            }
+
+            if (Verbose) Console.WriteLine(" = " + Math.Abs(result % 10));
+            if (Verbose) Console.ReadLine();
+
+            return Math.Abs(result % 10);
+        }
+
+        public long GetMultiplierFor(long positionConsidered, int element)
+        {
+            // posCon0:  0, 1, 0, -1, elementen die 1 pakken: 0, 4, 8, 12, etc. = el mod 4 == 0 in (0)
+            // posCon1:  0, 0, 1, 1, 0, 0, -1, -1, elementen die 1 pakken: 1, 2, 9, 10, etc = (el mod 8) in (1, 2)
+            // posCon2:  0, 0, 0, 1, 1, 1, 0, 0, 0, -1, -1, -1, elementen die 1 pakken: 2, 3, 4, 14, 15, 16, etc => (el mod 12) in (2, 3, 4)
+
+            var positionInNonRepeatingPattern =
+                (element % ((positionConsidered + 1) * 4)) + 1;
+
+            bool conditionpos =
+                positionInNonRepeatingPattern >= (positionConsidered + 1) &&
+                positionInNonRepeatingPattern < 2 * (positionConsidered + 1)
+                ;
+            bool conditionneg =
+                positionInNonRepeatingPattern >= (positionConsidered + 1) * 3 &&
+                positionInNonRepeatingPattern < 4 * (positionConsidered + 1);
+
+            if (conditionpos) return 1;
+            else if (conditionneg) return -1;
+            else return 0;
+        }
+
         public long[] RunPhase()
         {
             var newProgram = new long[program.Length];
@@ -36,18 +93,13 @@ namespace Advent2019.Advent16
             return newProgram;
         }
 
-        bool Verbose = true;
-
         public long[] pattern = new long[] { 0, 1, 0, -1 };
 
         public string GetResult1()
         {
-            Verbose = false;
             for (int n = 0; n < 100; n++)
             {
-                program = RunPhase();
-                Console.Write(n.ToString().PadRight(4) + ": ");
-                Console.WriteLine(ProgramString(program.Length -32, program.Length));
+                program = RunP1Phase();
             }
 
             var result = ProgramString(0, 8);
