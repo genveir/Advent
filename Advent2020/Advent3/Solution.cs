58ecf9a09ac9b35bc09cca2c67a12c4ab442c1e5
Geerten Vink <maedaros@yahoo.com>
2020-12-03 06:18:16 +0100

    day 3

--- Advent2020/Advent3/Solution.cs
+++ Advent2020/Advent3/Solution.cs
@@ -8,36 +8,60 @@ namespace Advent2020.Advent3
 {
     public class Solution : ISolution
     {
-        List<ParsedInput> modules;
+        List<ParsedInput> slopes;
 
         public Solution(string input)
         {
             var lines = Input.GetInputLines(input).ToArray();
 
-            var inputParser = new InputParser<string>("line");
-
-            modules = lines.Select(line =>
+            slopes = new List<ParsedInput>();
+            for (int n = 0; n < lines.Length; n++)
             {
-                var pi = new ParsedInput();
-                //(pi) = inputParser.Parse(line);
-                return pi;
-            }).ToList();
+                slopes.Add(new ParsedInput(lines[n]));
+            }
         }
         public Solution() : this("Input.txt") { }
 
         public class ParsedInput
         {
+            string rawInput;
 
+            public ParsedInput(string input)
+            {
+                rawInput = input;
+            }
+
+            public bool HasTree(int x)
+            {
+                return rawInput[x % rawInput.Length] == '#';
+            }
         }
 
         public string GetResult1()
         {
-            return "";
+            return RunSlope(3, 1).ToString();
+        }
+
+        private long RunSlope(int xMod, int yMod)
+        {
+            int x = 0;
+            int y = 0;
+            int trees = 0;
+
+            for (int n = 0; n < slopes.Count; n++)
+            {
+                x += xMod;
+                y += yMod;
+                if (y < slopes.Count && slopes[y].HasTree(x)) trees++;
+            }
+
+            return trees;
         }
 
         public string GetResult2()
         {
-            return "";
+            return
+                (RunSlope(1, 1) * RunSlope(3, 1) * RunSlope(5, 1) * RunSlope(7, 1) * RunSlope(1, 2)).ToString();
         }
     }
 }
