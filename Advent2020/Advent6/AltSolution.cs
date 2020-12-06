ea2099f824b30933d147ba9db7fe491a0b64c8d8
Geerten Vink <maedaros@yahoo.com>
2020-12-06 10:09:25 +0100

    oh wait, I can do this now

--- Advent2020/Advent6/AltSolution.cs
+++ Advent2020/Advent6/AltSolution.cs
@@ -8,14 +8,7 @@ namespace Advent2020.Advent6
 {
     public class AltSolution : ISolution
     {
-        public object GetResult1()
-        {
-            return Input.GetInput("input").Split("\r\n\r\n").Select(group => group.Replace("\r\n", "").Distinct().Count()).Sum();
-        }
-
-        public object GetResult2()
-        {
-            return Input.GetInput("input").Split("\r\n\r\n").Select(group => group.Split("\r\n").Aggregate((p1, p2) => new string(p1.Intersect(p2).ToArray())).Length).Sum();
-        }
+        public object GetResult1() => Input.GetInput("input").Split("\r\n\r\n").Select(group => group.Replace("\r\n", "").Distinct().Count()).Sum();
+        public object GetResult2() => Input.GetInput("input").Split("\r\n\r\n").Select(group => group.Split("\r\n").Aggregate((p1, p2) => new string(p1.Intersect(p2).ToArray())).Length).Sum();
     }
 }
