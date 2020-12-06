93abf26451baa38a0e66941370111cc1a3e3045c
Geerten Vink <maedaros@yahoo.com>
2020-12-06 10:32:34 +0100

    another using gone

--- Advent2020/Advent6/AltSolution.cs
+++ Advent2020/Advent6/AltSolution.cs
@@ -1,11 +1,10 @@
-﻿using Advent2020.Shared;
-using System.Linq;
+﻿using System.Linq;
 
 namespace Advent2020.Advent6
 {
-    public class AltSolution : ISolution
+    public class AltSolution : Shared.ISolution
     {
-        public object GetResult1() => Input.GetInput("input").Split("\r\n\r\n").Select(group => group.Replace("\r\n", "").Distinct().Count()).Sum();
-        public object GetResult2() => Input.GetInput("input").Split("\r\n\r\n").Select(group => group.Split("\r\n").Aggregate((p1, p2) => new string(p1.Intersect(p2).ToArray())).Length).Sum();
+        public object GetResult1() => Shared.Input.GetInput("input").Split("\r\n\r\n").Select(group => group.Replace("\r\n", "").Distinct().Count()).Sum();
+        public object GetResult2() => Shared.Input.GetInput("input").Split("\r\n\r\n").Select(group => group.Split("\r\n").Aggregate((p1, p2) => new string(p1.Intersect(p2).ToArray())).Length).Sum();
     }
 }
