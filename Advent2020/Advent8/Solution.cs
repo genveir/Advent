6812b8f1ac2bbbaf7bbe5ba4f3b162d795871e34
Geerten Vink <maedaros@yahoo.com>
2020-12-07 22:36:04 +0100

    day 8 prep

---
+++ Advent2020/Advent8/Solution.cs
@@ -0,0 +1,42 @@
+﻿using Advent2020.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2020.Advent8
+{
+    public class Solution : ISolution
+    {
+        List<ParsedInput> modules;
+
+        public Solution(string input)
+        {
+            var lines = Input.GetInputLines(input).ToArray();
+
+            var inputParser = new InputParser<string>("line");
+
+            modules = lines.Select(line =>
+            {
+                var pi = new ParsedInput();
+                //(pi) = inputParser.Parse(line);
+                return pi;
+            }).ToList();
+        }
+        public Solution() : this("Input.txt") { }
+
+        public class ParsedInput
+        {
+        }
+
+        public object GetResult1()
+        {
+            return "";
+        }
+
+        public object GetResult2()
+        {
+            return "";
+        }
+    }
+}
