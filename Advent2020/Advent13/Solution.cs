853546f83c7fc10147b936d62c05ac1064e83cb2
Geerten Vink <maedaros@yahoo.com>
2020-12-13 09:56:46 +0100

    day 13, not finished

---
+++ Advent2020/Advent13/Solution.cs
@@ -0,0 +1,98 @@
+﻿using Advent2020.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2020.Advent13
+{
+    public class Solution : ISolution
+    {
+        public long arrivalTimestamp;
+        public List<Bus> buses = new List<Bus>();
+
+        public Solution(string input)
+        {
+            var lines = Input.GetInputLines(input).ToArray();
+
+            arrivalTimestamp = long.Parse(lines[0]);
+            var busses = lines[1].Split(",");
+
+            for (int n = 0; n < busses.Length; n++)
+            {
+                if (busses[n] != "x") buses.Add(new Bus(long.Parse(busses[n]), n));
+            }
+        }
+        public Solution() : this("Input.txt") { }
+
+        public class Bus
+        {
+            public long firstMomentItCanLeave = 0;
+            public long period;
+            public long leaveOffset;
+
+            public Bus(long period, long leaveOffset)
+            {
+                this.period = period;
+                this.leaveOffset = leaveOffset;
+            }
+
+            public long GetTimeToWait(long timeStamp)
+            {
+                var posMod = timeStamp % period;
+
+                return period - (posMod);
+            }
+
+            public override string ToString()
+            {
+                return $"Bus {period} offset {leaveOffset} fl {firstMomentItCanLeave}";
+            }
+        }
+
+        public object GetResult1()
+        {
+            long lowestTime = long.MaxValue;
+            long answer = 0;
+            for (int n = 0; n < buses.Count; n++)
+            {
+                var timeToWait = buses[n].GetTimeToWait(arrivalTimestamp);
+                if (timeToWait < lowestTime)
+                {
+                    lowestTime = timeToWait;
+                    answer = timeToWait * buses[n].period;
+                }
+            }
+
+            return answer;
+        }
+
+        public object GetResult2()
+        {
+            var theMagicBus = buses.Aggregate((one, two) => CreateMagicCombinerBus(one, two));
+
+            return theMagicBus.firstMomentItCanLeave;
+        }
+
+        private Bus CreateMagicCombinerBus(Bus one, Bus two)
+        {
+            Console.WriteLine("checking " + one);
+            Console.WriteLine("against " + two);
+
+            long timeStamp = one.firstMomentItCanLeave;
+            while(true)
+            {
+                var timeTowait = two.GetTimeToWait(timeStamp);
+                if (timeTowait == two.leaveOffset) break;
+
+                timeStamp += one.period;
+
+                if (timeStamp > 10_000_000_000_000_000) return null;
+            }
+
+            var period = one.period * two.period;
+
+            return new Bus(period, 0) { firstMomentItCanLeave = timeStamp };
+        }
+    }
+}
---
+++ Advent2020/Advent13/Tests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2020.Advent13
+{
+    class Tests
+    {
+        [TestCase("", "")]
+        public void Test1(string input, string output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult1());
+        }
+
+        const string sample1 = @"0
+7,13,x,x,59,x,31,19";
+
+        const string sample2 = @"0
+17,x,13,19";
+
+        const string sample3 = @"0
+67,7,59,61";
+
+        const string sample4 = @"0
+67,x,7,59,61";
+
+        const string sample5 = @"0
+67,7,x,59,61";
+
+        const string sample6 = @"0
+1789,37,47,1889";
+
+        [TestCase(sample1, 1068781)]
+        [TestCase(sample2, 3417)]
+        [TestCase(sample3, 754018)]
+        [TestCase(sample4, 779210)]
+        [TestCase(sample5, 1261476)]
+        [TestCase(sample6, 1202161486)]
+        public void Test2(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult2());
+        }
+    }
+}
