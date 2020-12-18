9d5ab7b0a3dda151d6ffe3e86cd6f4b47cbc2c27
Geerten Vink <maedaros@yahoo.com>
2020-12-18 17:23:49 +0100

    whatever

---
+++ Advent2020/Advent18/Solution.cs
@@ -0,0 +1,177 @@
+﻿using Advent2020.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Advent2020.Advent18
+{
+    public class Solution : ISolution
+    {
+        public IEnumerable<Expression> expressions;
+
+        public Solution(string input)
+        {
+            var lines = Input.GetInputLines(input).ToArray();
+
+            expressions = lines
+                .Select(line => ParseExpression(line))
+                .ToList();
+        }
+        public Solution() : this("Input.txt") { }
+
+        public static Expression ParseExpression(string stringRepresentation)
+        {
+            stringRepresentation = Regex.Replace(stringRepresentation, "\\s", "");
+
+            return ParseExpression(stringRepresentation, 0, stringRepresentation.Length);
+        }
+        public static Expression ParseExpression(string stringRepresentation, int start, int end)
+        {
+            if (end - start == 1) return new LongExpression(stringRepresentation[start] - 48);
+
+            int ellipses = 0;
+            for (int n = end - 1; n >= start; n--)
+            {
+                char current = stringRepresentation[n];
+
+                if (current == '(') ellipses++;
+                else if (current == ')') ellipses--;
+                else
+                {
+                    if (ellipses == 0)
+                    {
+                        if (current == '+') return new AddExpression(
+                            ParseExpression(stringRepresentation, start, n),
+                            ParseExpression(stringRepresentation, n + 1, end)
+                            );
+                        else if (current == '*') return new MultiplyExpression(
+                            ParseExpression(stringRepresentation, start, n),
+                            ParseExpression(stringRepresentation, n + 1, end)
+                            );
+                    }
+                }
+            }
+
+            return new UnarySubExpression(ParseExpression(stringRepresentation, start + 1, end - 1));
+        }
+
+        public interface Expression
+        {
+            long Evaluate();
+
+            Expression Rewrite();
+        }
+
+        public class UnarySubExpression : Expression
+        {
+            public Expression Expression;
+
+            public UnarySubExpression(Expression expression)
+            {
+                this.Expression = expression;
+            }
+
+            public long Evaluate() { return Expression.Evaluate(); }
+
+            public Expression Rewrite()
+            {
+                return new UnarySubExpression(Expression.Rewrite());
+            }
+
+            public override string ToString()
+            {
+                return "(" + Expression + ")";
+            }
+        }
+
+        public class LongExpression : Expression
+        {
+            public long Value;
+
+            public LongExpression(long value)
+            {
+                this.Value = value;
+            }
+
+            public long Evaluate() { return Value; }
+
+            public Expression Rewrite() => this;
+
+            public override string ToString()
+            {
+                return Value.ToString();
+            }
+        }
+
+        public class MultiplyExpression : Expression
+        {
+            public Expression left;
+            public Expression right;
+
+            public MultiplyExpression(Expression left, Expression right)
+            {
+                this.left = left;
+                this.right = right;
+            }
+
+            public long Evaluate() { return left.Evaluate() * right.Evaluate(); }
+
+            public Expression Rewrite()
+            {
+                var lAdd = left as AddExpression;
+
+                return new MultiplyExpression(left.Rewrite(), right.Rewrite());
+            }
+
+            public override string ToString()
+            {
+                return "(" + left + " * " + right + ")";
+            }
+        }
+
+        public class AddExpression : Expression
+        {
+            public Expression left;
+            public Expression right;
+
+            public AddExpression(Expression left, Expression right)
+            {
+                this.left = left;
+                this.right = right;
+            }
+
+            public long Evaluate() { return left.Evaluate() + right.Evaluate(); }
+
+            public Expression Rewrite()
+            {
+                var lMul = left as MultiplyExpression;
+
+                if (lMul != null)
+                {
+                    return new MultiplyExpression(lMul.left.Rewrite(), new AddExpression(lMul.right.Rewrite(), this.right.Rewrite()));
+                }
+
+                return new AddExpression(left.Rewrite(), right.Rewrite());
+            }
+
+            public override string ToString()
+            {
+                return "(" + left + " + " + right + ")";
+            }
+        }
+
+        public object GetResult1()
+        {
+            return expressions.Select(exp => exp.Evaluate()).Sum();
+        }
+
+        public object GetResult2()
+        {
+            for (int i = 0; i < 100; i++) expressions = expressions.Select(exp => exp.Rewrite());
+
+            return expressions.Select(exp => exp.Evaluate()).Sum();
+        }
+    }
+}
---
+++ Advent2020/Advent18/Tests.cs
@@ -0,0 +1,118 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent2020.Advent18
+{
+    class Tests
+    {
+        [TestCase("1 + 2 * 3 + 4 * 5 + 6", 71)]
+        [TestCase("2 * 3 + (4 * 5)", 26)]
+        [TestCase("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437)]
+        [TestCase("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240)]
+        [TestCase("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)]
+        public void Test1(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult1());
+        }
+
+        [TestCase("1 + 2 * 3 + 4 * 5 + 6", 231)]
+        [TestCase("2 * 3 + (4 * 5)", 46)]
+        [TestCase("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445)]
+        [TestCase("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669060)]
+        [TestCase("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340)]
+        public void Test2(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult2());
+        }
+
+        [Test]
+        public void CanParseSingleLong()
+        {
+            string expression = "1";
+
+            var parsed = Solution.ParseExpression(expression);
+
+            Assert.AreEqual(1, parsed.Evaluate());
+        }
+
+        [Test]
+        public void CanParseLongInParenthesis()
+        {
+            string expression = "(1)";
+
+            var parsed = Solution.ParseExpression(expression);
+
+            Assert.AreEqual(1, parsed.Evaluate());
+        }
+
+        [Test]
+        public void CanParseSum()
+        {
+            string expression = "1+2";
+
+            var parsed = Solution.ParseExpression(expression);
+
+            Assert.AreEqual(3, parsed.Evaluate());
+        }
+
+        [Test]
+        public void CanParseProduct()
+        {
+            string expression = "1*2";
+
+            var parsed = Solution.ParseExpression(expression);
+
+            Assert.AreEqual(2, parsed.Evaluate());
+        }        
+
+        [Test]
+        public void CanParseMultipleOperators()
+        {
+            string expression = "1+1+1";
+
+            var parsed = Solution.ParseExpression(expression);
+
+            Assert.AreEqual(3, parsed.Evaluate());
+        }
+
+        [Test]
+        public void CanParseSubExpressions()
+        {
+            string expression = "(1*1)+(1*1)";
+
+            var parsed = Solution.ParseExpression(expression);
+
+            Assert.AreEqual(2, parsed.Evaluate());
+        }
+
+        [Test]
+        public void ParsingIsLeftAssociative()
+        {
+            var expression = Solution.ParseExpression("1+2*3");
+            var exprLeft = Solution.ParseExpression("(1+2)*3");
+            var exprRight = Solution.ParseExpression("1+(2*3)");
+
+            Assert.AreNotEqual(exprRight.Evaluate(), expression.Evaluate());
+            Assert.AreEqual(exprLeft.Evaluate(), expression.Evaluate());
+            Assert.AreEqual(9, expression.Evaluate());
+        }
+
+        [Test]
+        public void CanRewrite()
+        {
+            var expression = Solution.ParseExpression("6 * 2 + 3");
+
+            Assert.AreEqual(15, expression.Evaluate());
+
+            var rewrite = expression.Rewrite();
+
+            Assert.AreEqual(30, rewrite.Evaluate());
+        }
+    }
+}
