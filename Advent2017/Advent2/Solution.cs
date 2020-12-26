92806fef2256504114b1e22614ab050f33cf2d37
Geerten Vink <maedaros@yahoo.com>
2020-12-26 23:29:00 +0100

    day 2

---
+++ Advent2017/Advent2/Solution.cs
@@ -0,0 +1,50 @@
+﻿using Advent2017.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2017.Advent2
+{
+    public class Solution : ISolution
+    {
+        long[][] rows;
+
+        public Solution(string input)
+        {
+            var lines = Input.GetInputLines(input).ToArray();
+
+            rows = lines.Select(line => line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)).Select(longs => longs.Select(l => long.Parse(l)).ToArray()).ToArray();
+        }
+        public Solution() : this("Input.txt") { }
+
+        public object GetResult1()
+        {
+            long sum = 0;
+            foreach (var row in rows) sum += (row.Max() - row.Min());
+
+            return sum;
+        }
+
+        public object GetResult2()
+        {
+            long sum = 0;
+            foreach (var row in rows)
+            {
+                for (int first = 0; first < row.Length; first++)
+                {
+                    for (int second = first + 1; second < row.Length; second++)
+                    {
+                        long larger, smaller;
+                        if (row[first] >= row[second]) { larger = row[first]; smaller = row[second]; }
+                        else { larger = row[second]; smaller = row[first]; }
+
+                        var div = larger / smaller;
+                        if (smaller * div == larger) sum += div;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Advent2017/Advent2/Tests.cs
@@ -0,0 +1,36 @@
+﻿using Advent2017.Shared;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2017.Advent2
+{
+    class Tests
+    {
+        const string sample = @"5 1 9 5
+7 5 3
+2 4 6 8";
+
+        [TestCase(sample, 18)]
+        public void Test1(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult1());
+        }
+
+        const string sample2 = @"5 9 2 8
+9 4 7 3
+3 8 6 5";
+
+        [TestCase(sample2, 9)]
+        public void Test2(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult2());
+        }
+    }
+}
