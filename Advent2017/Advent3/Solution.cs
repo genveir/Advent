e2b5a7224f859f34a4bb737d889d5b45dc2e8228
Geerten Vink <maedaros@yahoo.com>
2020-12-27 00:12:37 +0100

    day 3 pt1

---
+++ Advent2017/Advent3/Solution.cs
@@ -0,0 +1,56 @@
+﻿using Advent2017.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2017.Advent3
+{
+    public class Solution : ISolution
+    {
+        public Solution(string input)
+        {
+            
+        }
+        public Solution() : this("Input.txt") { }
+
+        public Coordinate FindNum(long numToFind)
+        {
+            long n = 1;
+            long square = 1;
+            while(square < numToFind)
+            {
+                n += 2;
+                square = n * n;
+            }
+
+            var nSteps = (n - 1) / 2;
+            var squareCoord = new Coordinate(nSteps, nSteps);
+            var shiftLength = n - 1;
+
+            var corner = square;
+
+            if (corner - numToFind > shiftLength) { squareCoord = squareCoord.ShiftX(-shiftLength); corner -= shiftLength; }
+            else return squareCoord.ShiftX(numToFind - corner); 
+
+            if (corner - numToFind > shiftLength) { squareCoord = squareCoord.ShiftY(-shiftLength); corner -= shiftLength; }
+            else return squareCoord.ShiftY(numToFind - corner);
+
+            if (corner - numToFind > shiftLength) { squareCoord = squareCoord.ShiftX(shiftLength); corner -= shiftLength; }
+            else return squareCoord.ShiftX(corner - numToFind);
+
+            return squareCoord.ShiftY(corner - numToFind);
+        }
+
+        public object GetResult1()
+        {
+            var coord = FindNum(277678);
+            return coord.ManhattanDistance(new Coordinate(0,0));
+        }
+
+        public object GetResult2()
+        {
+            return "";
+        }
+    }
+}
---
+++ Advent2017/Advent3/Tests.cs
@@ -0,0 +1,31 @@
+﻿using Advent2017.Shared;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2017.Advent3
+{
+    class Tests
+    {
+        [TestCase("1", 0)]
+        [TestCase("12", 3)]
+        [TestCase("23", 2)]
+        [TestCase("1024", 31)]
+        public void Test1(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.FindNum(long.Parse(input)).ManhattanDistance(new Coordinate(0,0)));
+        }
+
+        [TestCase("", "")]
+        public void Test2(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult2());
+        }
+    }
+}
