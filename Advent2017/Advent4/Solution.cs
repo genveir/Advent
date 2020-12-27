030fe1b82e9a4bd7054cc2d90f26bdb4b82b59e4
Geerten Vink <maedaros@yahoo.com>
2020-12-27 16:00:47 +0100

    day 4

---
+++ Advent2017/Advent4/Solution.cs
@@ -0,0 +1,37 @@
+﻿using Advent2017.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2017.Advent4
+{
+    public class Solution : ISolution
+    {
+        string[][] words;
+
+        public Solution(string input)
+        {
+            var lines = Input.GetInputLines(input).ToArray();
+
+            words = lines.Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries)).ToArray();
+        }
+        public Solution() : this("Input.txt") { }
+
+        public object GetResult1()
+        {
+            return words
+                .Where(w => w.Count() == w.Distinct().Count())
+                .Count();
+        }
+
+        public object GetResult2()
+        {
+            return words
+                .Select(w => w.Select(chr => new string(chr.OrderBy(c => c).ToArray())))
+                .Where(w => w.Count() == w.Distinct().Count())
+                .Count();
+
+        }
+    }
+}
---
+++ Advent2017/Advent4/Tests.cs
@@ -0,0 +1,28 @@
+﻿using Advent2017.Shared;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2017.Advent4
+{
+    class Tests
+    {
+        [TestCase("", "")]
+        public void Test1(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult1());
+        }
+
+        [TestCase("", "")]
+        public void Test2(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult2());
+        }
+    }
+}
