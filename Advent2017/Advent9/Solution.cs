fda1eca81105eb496895a916d0c6bf5dfb972191
Geerten Vink <maedaros@yahoo.com>
2020-12-27 22:48:27 +0100

    day 9

---
+++ Advent2017/Advent9/Solution.cs
@@ -0,0 +1,88 @@
+﻿using Advent2017.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2017.Advent9
+{
+    public class Solution : ISolution
+    {
+        Group outerGroup;
+
+        public Solution(string input)
+        {
+            var stream = Input.GetInput(input);
+
+            int index = 0;
+            outerGroup = Group.Parse(stream, ref index, 1);
+        }
+        public Solution() : this("Input.txt") { }
+
+        public interface StreamItem 
+        {
+            long GetSumValue();
+            long GetGarbageLength();
+        }
+
+        public class Group : StreamItem
+        {
+            public List<StreamItem> items = new List<StreamItem>();
+            public long Value { get; set; }
+
+            public long GetSumValue() => Value + items.Select(i => i.GetSumValue()).Sum();
+            public long GetGarbageLength() => items.Select(i => i.GetGarbageLength()).Sum();
+
+            public static Group Parse(string input, ref int index, int value)
+            {
+                index++;
+                Group newGroup = new Group() { Value = value };
+                while (true)
+                {
+                    switch (input[index])
+                    {
+                        case '}': return newGroup;
+                        case '{': newGroup.items.Add(Group.Parse(input, ref index, value + 1)); break;
+                        case '<': newGroup.items.Add(Garbage.Parse(input, ref index)); break;
+                    }
+                    index++;
+                }
+            }
+        }
+
+        public class Garbage : StreamItem
+        {
+            public string content;
+
+            public long GetSumValue() => 0;
+            public long GetGarbageLength() => content.Length;
+
+            public static Garbage Parse(string input, ref int index)
+            {
+                index++;
+                List<char> content = new List<char>();
+
+                while (true)
+                {
+                    switch(input[index])
+                    {
+                        case '!': index++; break;
+                        case '>': return new Garbage() { content = new string(content.ToArray()) };
+                        default: content.Add(input[index]); break;
+                    }
+                    index++;
+                }
+            }
+        }
+
+        public object GetResult1()
+        {
+            return outerGroup.GetSumValue();
+        }
+
+        public object GetResult2()
+        {
+            return outerGroup.GetGarbageLength();
+        }
+    }
+}
---
+++ Advent2017/Advent9/Tests.cs
@@ -0,0 +1,40 @@
+﻿using Advent2017.Shared;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2017.Advent9
+{
+    class Tests
+    {
+        [TestCase("{}", 1)]
+        [TestCase("{{{}}}", 6)]
+        [TestCase("{{},{}}", 5)]
+        [TestCase("{{{},{},{{}}}}", 16)]
+        [TestCase("{{<ab>},{<ab>},{<ab>},{<ab>}}", 9)]
+        [TestCase("{{<!!>},{<!!>},{<!!>},{<!!>}}", 9)]
+        [TestCase("{{<a!>},{<a!>},{<a!>},{<ab>}}", 3)]
+        public void Test1(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult1());
+        }
+
+        [TestCase("{<>}", 0)]
+        [TestCase("{<random characters>}", 17)]
+        [TestCase("{<<<<>}", 3)]
+        [TestCase("{<{!>}>}", 2)]
+        [TestCase("{<!!> }", 0)]
+        [TestCase("{<!!!>> }", 0)]
+        [TestCase("{<{o\"i!a,<{i<a>}", 10)]
+        public void Test2(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult2());
+        }
+    }
+}
