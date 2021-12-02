d17c8a06fb9706a250faab68887b5de8644b5c47
Geerten Vink <maedaros@yahoo.com>
2021-12-02 06:43:24 +0100

    day 3 prep

---
+++ Advent2021/Advent03/Solution.cs
@@ -0,0 +1,45 @@
+﻿using Advent2021.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2021.Advent03
+{
+    public class Solution : ISolution
+    {
+        List<ParsedInput> modules;
+
+        public Solution(string input)
+        {
+            var lines = Input.GetInputLines(input).ToArray();
+
+            var inputParser = new InputParser<string, string>("var1 var2");
+
+            modules = lines.Select(line =>
+            {
+                var (var1, var2) = inputParser.Parse(line);
+                return new ParsedInput();
+            }).ToList();
+        }
+        public Solution() : this("Input.txt") { }
+
+        public class ParsedInput
+        {
+            public ParsedInput()
+            {
+
+            }
+        }
+
+        public object GetResult1()
+        {
+            return "";
+        }
+
+        public object GetResult2()
+        {
+            return "";
+        }
+    }
+}
