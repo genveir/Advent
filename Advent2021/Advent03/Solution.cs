0d10690a470487762c41b0ccc8d4ccf6c7529083
Geerten Vink <maedaros@yahoo.com>
2021-12-03 06:42:00 +0100

    day 3.. more struggles

--- Advent2021/Advent03/Solution.cs
+++ Advent2021/Advent03/Solution.cs
@@ -8,38 +8,124 @@ namespace Advent2021.Advent03
 {
     public class Solution : ISolution
     {
-        List<ParsedInput> modules;
+        string[] numbers;
 
         public Solution(string input)
         {
-            var lines = Input.GetInputLines(input).ToArray();
-
-            var inputParser = new InputParser<string, string>("var1 var2");
-
-            modules = lines.Select(line =>
-            {
-                var (var1, var2) = inputParser.Parse(line);
-                return new ParsedInput();
-            }).ToList();
+            numbers = Input.GetInputLines(input).ToArray();
+            
         }
         public Solution() : this("Input.txt") { }
 
-        public class ParsedInput
+        public long P1()
         {
-            public ParsedInput()
-            {
+            char[] gamma = GetGamma(numbers);
+            char[] episilon = GetEpsilon(numbers);
+            
+            string gammaString = new string(gamma);
+            string epsilonString = new string(episilon);
 
+            return Convert.ToInt64(gammaString, 2) * Convert.ToInt64(epsilonString, 2);
+        }
+
+        public char[] GetGamma(IEnumerable<string> input)
+        {
+            var numbers = input.ToArray();
+
+            char[] gamma = new char[numbers[0].Length];
+            for (int n = 0; n < numbers[0].Length; n++)
+            {
+                int numOnes = 0;
+                int numZeros = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i][n] == '1') numOnes++;
+                    else numZeros++;
+                }
+
+                gamma[n] = (numOnes > numZeros) ? '1' : '0';
+                if (numOnes == numZeros) gamma[n] = '1';
             }
+
+            return gamma;
+        }
+
+        public char[] GetEpsilon(IEnumerable<string> input)
+        {
+            var numbers = input.ToArray();
+
+            char[] gamma = new char[numbers[0].Length];
+            for (int n = 0; n < numbers[0].Length; n++)
+            {
+                int numOnes = 0;
+                int numZeros = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i][n] == '0') numOnes++;
+                    else numZeros++;
+                }
+
+                gamma[n] = (numOnes > numZeros) ? '1' : '0';
+                if (numOnes == numZeros) gamma[n] = '0';
+            }
+
+            return gamma;
+        }
+
+        public long P2()
+        {
+            List<string> oxy = numbers.ToList();
+            List<string> co2 = numbers.ToList();
+
+            for (int n = 0; n < oxy[0].Length; n++)
+            {
+                char[] gamma = GetGamma(oxy);
+                char[] episilon = GetEpsilon(co2);
+
+                List<string> nextOxy = new List<string>();
+                List<string> nextCo2 = new List<string>();
+
+                if (oxy.Count == 1)
+                {
+                    nextOxy.Add(oxy.Single());
+                }
+                else
+                {
+                    for (int i = 0; i < oxy.Count; i++)
+                    {
+                        if (oxy[i][n] == gamma[n]) nextOxy.Add(oxy[i]);
+                    }
+                }
+                oxy = nextOxy;
+
+                if (co2.Count == 1)
+                {
+                    nextCo2.Add(co2.Single());
+                }
+                else
+                {
+                    for (int i = 0; i < co2.Count; i++)
+                    {
+                        if (co2[i][n] == episilon[n]) nextCo2.Add(co2[i]);
+                    }
+                }
+                co2 = nextCo2;
+            }
+
+            var oxyString = oxy.Single();
+            var co2String = co2.Single();
+
+            return Convert.ToInt64(oxyString, 2) * Convert.ToInt64(co2String, 2);
         }
 
         public object GetResult1()
         {
-            return "";
+            return P1();
         }
 
         public object GetResult2()
         {
-            return "";
+            return P2();
         }
     }
 }
