889c416b4d1f65fd1f2ad895f24f3758aff52350
Geerten Vink <maedaros@yahoo.com>
2021-12-03 12:42:30 +0100

    removed division

--- Advent2021/Advent03/Solution.cs
+++ Advent2021/Advent03/Solution.cs
@@ -12,27 +12,22 @@ namespace Advent2021.Advent03
 
         public Solution(string input)
         {
-            numbers = Input
-                .GetInputLines(input)
-                .Select(s => s.AsDigits())
-                .Pivot();
+            numbers = Input.GetInputLines(input).Select(s => s.AsDigits()).Pivot();
         }
         public Solution() : this("Input.txt") { }
 
         public static int[] GetMatch(IEnumerable<IEnumerable<int>> input, int match) =>
-            input.Select(inp => (inp.Sum() >= inp.Count() / 2.0d) ? match : 1 - match).ToArray();
+            input.Select(inp => (inp.Sum() * 2 >= inp.Count()) ? match : 1 - match).ToArray();
 
         public static List<int> GetGasMatch(IEnumerable<IEnumerable<int>> input, int match, int digit)
         {
-            var gases = input.Pivot();
-
             var matchNum = GetMatch(input, match)[digit];
-            var nextSet = gases.Where(gas => gas[digit] == matchNum);
+            var nextSet = input.Pivot().Where(gas => gas[digit] == matchNum);
 
             return nextSet.Count() == 1 ? nextSet.Single() : GetGasMatch(nextSet.Pivot(), match, digit + 1);
         }
 
-        public long CalculateResult(Func<int, IEnumerable<int>> matchFunc) =>
+        public static long CalculateResult(Func<int, IEnumerable<int>> matchFunc) =>
             Enumerable.Range(0, 2)
                 .Select(matchFunc)
                 .Select(ints => Convert.ToInt64(string.Join("", ints), 2))
