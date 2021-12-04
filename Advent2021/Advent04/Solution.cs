83bff02a30aaef1b67a4c8cb5ae51344f3693509
Geerten Vink <maedaros@yahoo.com>
2021-12-04 06:42:33 +0100

    actual code. tests fail, no idea why the answers are right.

--- Advent2021/Advent04/Solution.cs
+++ Advent2021/Advent04/Solution.cs
@@ -8,35 +8,128 @@ namespace Advent2021.Advent04
 {
     public class Solution : ISolution
     {
-        List<ParsedInput> modules;
+        long[] numbers;
+        BingoBoard[] bingoBoards;
+
+        public static string numberInput = "63,23,2,65,55,94,38,20,22,39,5,98,9,60,80,45,99,68,12,3,6,34,64,10,70,69,95,96,83,81,32,30,42,73,52,48,92,28,37,35,54,7,50,21,74,36,91,97,13,71,86,53,46,58,76,77,14,88,78,1,33,51,89,26,27,31,82,44,61,62,75,66,11,93,49,43,85,0,87,40,24,29,15,59,16,67,19,72,57,41,8,79,56,4,18,17,84,90,47,25";
 
         public Solution(string input)
         {
-            var lines = Input.GetInputLines(input).ToArray();
+            numbers = numberInput.Split(',').Select(long.Parse).ToArray();
 
-            var inputParser = new InputParser<string>("line");
+            var blocks = Input.GetBlockLines(input).ToArray();
 
-            modules = lines.Select(line =>
-            {
-                var pi = new ParsedInput();
-                //(pi) = inputParser.Parse(line);
-                return pi;
-            }).ToList();
+            bingoBoards = blocks.Select(b => new BingoBoard(b)).ToArray();
         }
         public Solution() : this("Input.txt") { }
 
-        public class ParsedInput
+        public class BingoBoard
         {
+            public long[][] fields;
+            public bool[][] picked;
+
+            Dictionary<long, Coordinate> byNum = new Dictionary<long, Coordinate>();
+
+            public BingoBoard(string[] input)
+            {
+                fields = new long[5][];
+                picked = new bool[5][];
+
+                for (int y = 0; y < 5; y++)
+                {
+                    fields[y] = new long[5];
+                    picked[y] = new bool[5];
+
+                    var nums = input[y].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    for (int x =0; x < 5; x++)
+                    {
+                        var num = long.Parse(nums[x]);
+                        fields[y][x] = num;
+
+                        var coord = new Coordinate(x, y);
+                        byNum.Add(num, coord);
+                    }
+                }
+            }
+
+            public void Pick(long number)
+            {
+                Coordinate coordinate;
+                if (byNum.TryGetValue(number, out coordinate))
+                {
+                    picked[coordinate.Y][coordinate.X] = true;
+                }
+            }
+
+            public bool CheckForWin()
+            {
+                var pivotted = picked.Pivot();
+
+                for (int y = 0; y < 5; y++) if (picked[y].All(p => p)) return true;
+                for (int x = 0; x < 5; x++) if (pivotted[x].All(p => p)) return true;
+
+                return false;
+            }
+
+            public long score = -1; // urgl
+            public long turn;
+            public void MarkWin(long turn, long number)
+            {
+                if (this.score != -1) return;
+
+                this.turn = turn;
+                this.score = number * CalculateScore();
+            }
+
+            public long CalculateScore()
+            {
+                long result = 0;
+                for (int y = 0; y < 5; y++)
+                {
+                    for (int x = 0; x < 5; x++)
+                    {
+                        if (!picked[y][x]) result += fields[y][x];
+                    }
+                }
+                return result;
+            }
         }
 
         public object GetResult1()
         {
-            return "";
+            for (int n = 0; n < numbers.Length; n++)
+            {
+                var number = numbers[n];
+
+                foreach (var board in bingoBoards)
+                {
+                    board.Pick(number);
+
+                    if (board.CheckForWin()) return board.CalculateScore() * number;
+                }
+            }
+
+            return "no answer";
         }
 
         public object GetResult2()
         {
-            return "";
+            for (int n = 0; n < numbers.Length; n++)
+            {
+                var number = numbers[n];
+
+                foreach (var board in bingoBoards)
+                {
+                    board.Pick(number);
+
+                    if (board.CheckForWin()) board.MarkWin(n, number);
+                }
+            }
+
+            var last = bingoBoards.OrderBy(b => b.turn).Last();
+
+            return last.score;
         }
     }
 }
--- Advent2021/Advent04/Tests.cs
+++ Advent2021/Advent04/Tests.cs
@@ -9,20 +9,42 @@ namespace Advent2021.Advent04
 {
     class Tests
     {
-        [TestCase("", "")]
-        public void Test1(string input, object output)
+        [TestCase(example, exampleNums, 4512)]
+        public void Test1(string input, string numberList, object output)
         {
             var sol = new Solution(input);
+            Solution.numberInput = numberList;
 
             Assert.AreEqual(output, sol.GetResult1());
         }
 
-        [TestCase("", "")]
-        public void Test2(string input, object output)
+        [TestCase(example, exampleNums, 1924)]
+        public void Test2(string input, string numberList, object output)
         {
             var sol = new Solution(input);
+            Solution.numberInput = numberList;
 
             Assert.AreEqual(output, sol.GetResult2());
         }
+
+        public const string exampleNums = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1";
+
+        public const string example = @"22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7";
     }
 }
