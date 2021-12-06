f347e0ae4c1e26707c1783d548e5037989953d22
Geerten Vink <maedaros@yahoo.com>
2021-12-06 06:44:27 +0100

    actual day9

--- Advent2021/Advent06/Solution.cs
+++ Advent2021/Advent06/Solution.cs
@@ -8,35 +8,82 @@ namespace Advent2021.Advent06
 {
     public class Solution : ISolution
     {
-        List<ParsedInput> modules;
+        List<long> numbers;
+        public List<LanternFish> fishes;
+
+        public long[] fishArray = new long[9];
 
         public Solution(string input)
         {
-            var lines = Input.GetInputLines(input).ToArray();
+            numbers = Input.GetNumbers(input, ',').ToList();
 
-            var inputParser = new InputParser<ParsedInput>("line");
-
-            modules = inputParser.Parse(lines);
+            fishes = numbers.Select(n => new LanternFish(n)).ToList();
+            foreach (var n in numbers) fishArray[n]++;
         }
         public Solution() : this("Input.txt") { }
 
-        public class ParsedInput
+        public class LanternFish
         {
-            [ComplexParserConstructor]
-            public ParsedInput()
-            {
+            public long currentAge;
 
+            public LanternFish(long currentAge)
+            {
+                this.currentAge = currentAge;
+            }
+
+            public void Tick(List<LanternFish> nextGen)
+            {
+                if (currentAge == 0)
+                {
+                    nextGen.Add(new LanternFish(8));
+                    currentAge = 7;
+                }
+
+                currentAge--;
+            }
+
+            public override string ToString()
+            {
+                return currentAge.ToString();
             }
         }
 
+        public static long days = 80;
         public object GetResult1()
         {
-            return "";
+            for (int n = 0; n < days; n++)
+            {
+                var nextGen = new List<LanternFish>();
+
+                foreach(var fish in fishes)
+                {
+                    fish.Tick(nextGen);
+                }
+                fishes.AddRange(nextGen);
+                ;
+            }
+            return fishes.Count;
         }
 
+        public static long days2 = 256;
         public object GetResult2()
         {
-            return "";
+            for (int n = 0; n < days2; n++)
+            {
+                var newArray = new long[9];
+                newArray[0] = fishArray[1];
+                newArray[1] = fishArray[2];
+                newArray[2] = fishArray[3];
+                newArray[3] = fishArray[4];
+                newArray[4] = fishArray[5];
+                newArray[5] = fishArray[6];
+                newArray[6] = fishArray[7] + fishArray[0];
+                newArray[7] = fishArray[8];
+                newArray[8] = fishArray[0];
+
+                fishArray = newArray;
+            }
+            return fishArray.Sum();
         }
     }
 }
--- Advent2021/Advent06/Tests.cs
+++ Advent2021/Advent06/Tests.cs
@@ -4,27 +4,73 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using static Advent2021.Advent06.Solution;
 
 namespace Advent2021.Advent06
 {
     class Tests
     {
-        [TestCase(example, "")]
-        public void Test1(string input, object output)
+        [TestCase(example, 18, 26)]
+        [TestCase(example, 80, 5934)]
+        public void Test1(string input, long days, object output)
         {
             var sol = new Solution(input);
+            Solution.days = days;
 
             Assert.AreEqual(output, sol.GetResult1());
         }
 
-        [TestCase(example, "")]
-        public void Test2(string input, object output)
+        [TestCase(example, 18, 26)]
+        [TestCase(example, 80, 5934)]
+        [TestCase(example, 256, 26984457539)]
+        public void Test2(string input, long days, object output)
         {
             var sol = new Solution(input);
+            Solution.days2 = days;
 
             Assert.AreEqual(output, sol.GetResult2());
         }
 
-        public const string example = @"";
-    }
+        [Test]
+        public void WaarGaatHetFoutDan()
+        {
+            var inputParser = new InputParser<long, long[]>(false, 2, new string[] { "After", "days:" });
+
+            var expected = Input.GetInputLines(output).ToArray();
+
+            var solution = new Solution(example);
+            for (int n = 0; n < 18; n++)
+            {
+                var nextGen = new List<LanternFish>();
+
+                foreach (var fish in solution.fishes)
+                {
+                    fish.Tick(nextGen);
+                }
+                solution.fishes.AddRange(nextGen);
+
+                Assert.AreEqual(expected[n], string.Join(',', solution.fishes));
+            }
+        }
+
+        public const string example = @"3,4,3,1,2";
+
+        public const string output = @"2,3,2,0,1
+1,2,1,6,0,8
+0,1,0,5,6,7,8
+6,0,6,4,5,6,7,8,8
+5,6,5,3,4,5,6,7,7,8
+4,5,4,2,3,4,5,6,6,7
+3,4,3,1,2,3,4,5,5,6
+2,3,2,0,1,2,3,4,4,5
+1,2,1,6,0,1,2,3,3,4,8
+0,1,0,5,6,0,1,2,2,3,7,8
+6,0,6,4,5,6,0,1,1,2,6,7,8,8,8
+5,6,5,3,4,5,6,0,0,1,5,6,7,7,7,8,8
+4,5,4,2,3,4,5,6,6,0,4,5,6,6,6,7,7,8,8
+3,4,3,1,2,3,4,5,5,6,3,4,5,5,5,6,6,7,7,8
+2,3,2,0,1,2,3,4,4,5,2,3,4,4,4,5,5,6,6,7
+1,2,1,6,0,1,2,3,3,4,1,2,3,3,3,4,4,5,5,6,8
+0,1,0,5,6,0,1,2,2,3,0,1,2,2,2,3,3,4,4,5,7,8
+6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8";
 }
