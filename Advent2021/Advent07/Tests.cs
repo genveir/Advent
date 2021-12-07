0a97c4b917aa8343cbe69becb47764bc635f6a64
Geerten Vink <maedaros@yahoo.com>
2021-12-07 05:53:00 +0100

    day 7 prep

---
+++ Advent2021/Advent07/Tests.cs
@@ -0,0 +1,30 @@
+﻿using Advent2021.Shared;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2021.Advent07
+{
+    class Tests
+    {
+        [TestCase(example, "")]
+        public void Test1(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult1());
+        }
+   
+        [TestCase(example, "")]
+        public void Test2(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult2());
+        }
+
+        public const string example = @"";
+    }
+}
