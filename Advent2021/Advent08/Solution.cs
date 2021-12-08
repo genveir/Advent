e4b6e01b861115bf90a09f4994fdcb0e335edac2
Geerten Vink <maedaros@yahoo.com>
2021-12-08 07:19:43 +0100

    actual day8.. myeah.

--- Advent2021/Advent08/Solution.cs
+++ Advent2021/Advent08/Solution.cs
@@ -14,7 +14,8 @@ namespace Advent2021.Advent08
         {
             var lines = Input.GetInputLines(input).ToArray();
 
-            var inputParser = new InputParser<ParsedInput>("line");
+            var inputParser = new InputParser<ParsedInput>("words | words");
+            inputParser.ArrayDelimiters = new char[] { ' ' };
 
             modules = inputParser.Parse(lines);
         }
@@ -22,21 +23,121 @@ namespace Advent2021.Advent08
 
         public class ParsedInput
         {
-            [ComplexParserConstructor]
-            public ParsedInput()
-            {
+            public string[] wires;
+            public string[] segments;
 
+            [ComplexParserConstructor]
+            public ParsedInput(string[] wires, string[] segments)
+            {
+                this.wires = wires.Select(w =>
+                {
+                    var orderedArray = w.OrderBy(c => c).ToArray();
+                    return new string(orderedArray);
+                }).ToArray();
+                this.segments = segments.Select(w =>
+                {
+                    var orderedArray = w.OrderBy(c => c).ToArray();
+                    return new string(orderedArray);
+                }).ToArray();
             }
-        }
+
+            public long Count()
+            {
+                return segments.Where(s => s.Length == 2 || s.Length == 3 || s.Length == 7 || s.Length == 4).Count();
+            }
+
+            public long GetValue()
+            {
+                var values = GetValues();
+
+                var digits = string.Join("", segments.Select(s => values[s]));
+
+                return long.Parse(digits);
+            }
+
+            public Dictionary<string, int> GetValues()
+            {
+                // 0 yes
+                // 1 yes
+                // 2 yes
+                // 3 yes
+                // 4 yes
+                // 5 yes
+                // 6 yes
+                // 7 yes
+                // 8 yes
+                // 9 yes
+
+                var allSegments = wires.Union(segments);
+                Console.WriteLine(allSegments.Count());
+
+                var one = allSegments.Single(s => s.Length == 2);
+                var seven = allSegments.Single(s => s.Length == 3);
+                var four = allSegments.Single(s => s.Length == 4);
+                var eight = allSegments.Single(s => s.Length == 7);
+
+                var twoThreeAndFive = allSegments.Where(s => s.Length == 5);
+                var zeroSixAndNine = allSegments.Where(s => s.Length == 6);
+
+                var cf = one;
+                var acf = seven;
+                var bcdf = four;
+                var a = acf.Except(cf);
+                var bd = bcdf.Except(cf);
+
+                // 3
+                var acdfg = twoThreeAndFive.Where(n => n.Contains(cf[0]) && n.Contains(cf[1])).Single();
+                var three = acdfg;
+
+                var twoAndFive = twoThreeAndFive.Except(new List<string>() { three });
+
+                var adg = acdfg.Except(cf);
+                var dg = adg.Except(a);
+
+                var d = dg.Intersect(bd);
+                var b = bd.Except(d);
+                var g = dg.Except(d);
+
+                // 0
+                var abcefg = eight.Except(d);
+                var zero = new string(abcefg.ToArray());
+
+                var sixAndNine = zeroSixAndNine.Except(new List<string>() { zero });
+
+                var five = twoAndFive.Where(n => n.Contains(b.Single())).Single();
+                var two = twoAndFive.Except( new List<string>() { five });
+
+                var acdeg = two.Single().ToArray();
+
+                var e = acdeg.Except(acdfg).Except(cf);
+
+                var six = sixAndNine.Where(n => n.Contains(e.Single())).Single();
+                var nine = sixAndNine.Except(new List<string>() { six });
+
+                var dict = new Dictionary<string, int>();
+                dict.Add(zero, 0);
+                dict.Add(one, 1);
+                dict.Add(two.Single(), 2);
+                dict.Add(three, 3);
+                dict.Add(four, 4);
+                dict.Add(five, 5);
+                dict.Add(six, 6);
+                dict.Add(seven, 7);
+                dict.Add(eight, 8);
+                dict.Add(nine.Single(), 9);
+
+                return dict;
+            }
+        }        
 
         public object GetResult1()
         {
-            return "";
+            return modules.Sum(m => m.Count());
         }
 
         public object GetResult2()
         {
-            return "";
+            return modules.Sum(m => m.GetValue());
         }
     }
 }
--- Advent2021/Advent08/Tests.cs
+++ Advent2021/Advent08/Tests.cs
@@ -17,7 +17,8 @@ namespace Advent2021.Advent08
             Assert.AreEqual(output, sol.GetResult1());
         }
 
-        [TestCase(example, "")]
+        //[TestCase(example, 61229)]
+        [TestCase(bestExample, 1111)]
         public void Test2(string input, object output)
         {
             var sol = new Solution(input);
@@ -25,6 +26,17 @@ namespace Advent2021.Advent08
             Assert.AreEqual(output, sol.GetResult2());
         }
 
-        public const string example = @"";
+        public const string bestExample = @"abcefg cf acdeg acdfg bcdf abdfg abdefg acf abcdefg abcdfg | cf cf cf cf";
+
+        public const string example = @"be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce";
     }
 }
