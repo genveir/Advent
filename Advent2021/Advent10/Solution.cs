ebdf1c1d483ee72ac4aa38635792b4f82a314e1b
Geerten Vink <maedaros@yahoo.com>
2021-12-09 20:09:00 +0100

    day 10 prep

---
+++ Advent2021/Advent10/Solution.cs
@@ -0,0 +1,42 @@
+﻿using Advent2021.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2021.Advent10
+{
+    public class Solution : ISolution
+    {
+        List<ParsedInput> modules;
+
+        public Solution(string input)
+        {
+            var lines = Input.GetInputLines(input).ToArray();
+
+            var inputParser = new InputParser<ParsedInput>("line");
+
+            modules = inputParser.Parse(lines);
+        }
+        public Solution() : this("Input.txt") { }
+
+        public class ParsedInput
+        {
+            [ComplexParserConstructor]
+            public ParsedInput()
+            {
+
+            }
+        }
+
+        public object GetResult1()
+        {
+            return "";
+        }
+
+        public object GetResult2()
+        {
+            return "";
+        }
+    }
+}
