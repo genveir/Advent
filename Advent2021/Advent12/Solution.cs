4c7b9fe62bb7d38f7d6106b3ccd119b6148fd742
Geerten Vink <maedaros@yahoo.com>
2021-12-12 07:34:51 +0100

    Day 12 as is...

--- Advent2021/Advent12/Solution.cs
+++ Advent2021/Advent12/Solution.cs
@@ -8,35 +8,228 @@ namespace Advent2021.Advent12
 {
     public class Solution : ISolution
     {
-        List<ParsedInput> modules;
+        List<Cave> caves;
+        Dictionary<string, Cave> allCaves = new Dictionary<string, Cave>();
 
         public Solution(string input)
         {
             var lines = Input.GetInputLines(input).ToArray();
 
-            var inputParser = new InputParser<ParsedInput>("line");
+            var inputParser = new InputParser<ParsedInput>("a-b");
 
-            modules = inputParser.Parse(lines);
+            var links = inputParser.Parse(lines);
+
+            foreach (var link in links) link.SetCaves(allCaves); 
+
+            caves = allCaves.Values.ToList();
         }
         public Solution() : this("Input.txt") { }
 
         public class ParsedInput
         {
-            [ComplexParserConstructor]
-            public ParsedInput()
-            {
+            public string from;
+            public string to;
 
+            public Cave fromCave;
+            public Cave toCave;
+
+            [ComplexParserConstructor]
+            public ParsedInput(string from, string to)
+            {
+                this.from = from;
+                this.to = to;
+            }
+
+            public void SetCaves(Dictionary<string, Cave> allCaves)
+            {
+                if (!allCaves.TryGetValue(from, out fromCave))
+                {
+                    fromCave = new Cave(from);
+                    allCaves.Add(from, fromCave);
+                }
+                if (!allCaves.TryGetValue(to, out toCave))
+                {
+                    toCave = new Cave(to);
+                    allCaves.Add(to, toCave);
+                }
+
+                fromCave.LinkNeighbour(toCave);
+                toCave.LinkNeighbour(fromCave);
+            }
+        }
+
+        public class Cave
+        {
+            public string name;
+
+            public bool isBig;
+            public bool isTiny;
+
+            public List<Cave> neighbours = new List<Cave>();
+
+            public Cave(string name)
+            {
+                this.name = name;
+                isBig = name.ToUpper() == name;
+                isTiny = name == "start" || name == "end";
+
+                if (name == "end") isBig = true;
+            }
+
+            public void LinkNeighbour(Cave neighbour)
+            {
+                neighbours.Add(neighbour);
+            }
+
+            public override int GetHashCode()
+            {
+                return name.GetHashCode();
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(obj, this)) return true;
+                var other = obj as Cave;
+                return other.name == this.name;
+            }
+
+            public override string ToString()
+            {
+                return name;
+            }
+        }
+
+        public class SearchNode1
+        {
+            public HashSet<Cave> cantVisit = new HashSet<Cave>();
+            public Cave Current;
+
+            public SearchNode1(Cave start)
+            {
+                this.Current = start;
+            }
+
+            public SearchNode1(SearchNode1 parent, Cave newCurrent)
+            {
+                this.cantVisit = new HashSet<Cave>(parent.cantVisit);
+                this.Current = newCurrent;
+
+                if (!parent.Current.isBig) this.cantVisit.Add(parent.Current);
+            }
+
+            public IEnumerable<SearchNode1> Explore()
+            {
+                foreach (var neighbour in Current.neighbours)
+                {
+                    if (cantVisit.Contains(neighbour)) continue;
+                    else yield return new SearchNode1(this, neighbour);
+                }
+            }
+
+            public bool isWin => Current.name == "end";
+        }
+
+        public class SearchNode2
+        {
+            public List<Cave> inOrder = new List<Cave>();
+            public HashSet<Cave> smallCavesVisited = new HashSet<Cave>();
+            public HashSet<Cave> cantVisit = new HashSet<Cave>();
+            public bool cantVisitSmall = false;
+            public Cave Current;
+
+            public SearchNode2(Cave start) 
+            {
+                this.Current = start;
+                this.inOrder = new List<Cave>() { start };
+            }
+
+            public SearchNode2(SearchNode2 parent, Cave newCurrent)
+            {
+                this.inOrder = new List<Cave>(parent.inOrder);
+                this.inOrder.Add(newCurrent);
+
+                this.smallCavesVisited = new HashSet<Cave>(parent.smallCavesVisited); // small visited once
+                this.cantVisit = new HashSet<Cave>(parent.cantVisit); // small visited twice + start/end
+                this.cantVisitSmall = parent.cantVisitSmall;
+                this.Current = newCurrent;
+
+                if (parent.Current.isTiny) this.cantVisit.Add(parent.Current);
+                if (smallCavesVisited.Contains(Current)) this.cantVisitSmall = true;
+
+                if (!Current.isBig) smallCavesVisited.Add(Current);
+            }
+
+            public IEnumerable<SearchNode2> Explore()
+            {
+                foreach(var neighbour in Current.neighbours)
+                {
+                    if (cantVisit.Contains(neighbour)) continue;
+                    if (cantVisitSmall && !neighbour.isBig && smallCavesVisited.Contains(neighbour)) continue;
+                    else yield return new SearchNode2(this, neighbour);
+                }
+            }
+
+            public bool isWin => Current.name == "end";
+
+            public override string ToString()
+            {
+                //return $"node at {Current}, visited {string.Join(",", this.smallCavesVisited)}, can't visit: {string.Join(",", this.cantVisit)}";
+                return string.Join(",", inOrder);
+            }
+        }
+
+        public IEnumerable<SearchNode1> EnumeratePaths1()
+        {
+            var start = allCaves["start"];
+            var node = new SearchNode1(start);
+
+            var queue = new Queue<SearchNode1>();
+            queue.Enqueue(node);
+
+            while (queue.Count > 0)
+            {
+                var popped = queue.Dequeue();
+
+                if (popped.isWin) yield return popped;
+                else
+                {
+                    var newNodes = popped.Explore();
+                    foreach (var newNode in newNodes) queue.Enqueue(newNode);
+                }
+            }
+        }
+    
+
+        public IEnumerable<SearchNode2> EnumeratePaths2()
+        {
+            var start = allCaves["start"];
+            var node = new SearchNode2(start);
+
+            var queue = new Queue<SearchNode2>();
+            queue.Enqueue(node);
+
+            while (queue.Count > 0)
+            {
+                var popped = queue.Dequeue();
+
+                if (popped.isWin) yield return popped;
+                else
+                {
+                    var newNodes = popped.Explore();
+                    foreach (var newNode in newNodes) queue.Enqueue(newNode);
+                }
             }
         }
 
         public object GetResult1()
         {
-            return "";
+            return EnumeratePaths1().Count();
         }
 
         public object GetResult2()
         {
-            return "";
+            var allResults = EnumeratePaths2().ToList();
+            return allResults.Count;
         }
     }
 }
--- Advent2021/Advent12/Tests.cs
+++ Advent2021/Advent12/Tests.cs
@@ -9,7 +9,8 @@ namespace Advent2021.Advent12
 {
     class Tests
     {
-        [TestCase(example, "")]
+        [TestCase(example, 10)]
+        [TestCase(simpleCase, 2)]
         public void Test1(string input, object output)
         {
             var sol = new Solution(input);
@@ -17,7 +18,8 @@ namespace Advent2021.Advent12
             Assert.AreEqual(output, sol.GetResult1());
         }
 
-        [TestCase(example, "")]
+        [TestCase(example, 36)]
+        [TestCase(simpleCase, 3)]
         public void Test2(string input, object output)
         {
             var sol = new Solution(input);
@@ -25,6 +27,87 @@ namespace Advent2021.Advent12
             Assert.AreEqual(output, sol.GetResult2());
         }
 
-        public const string example = @"";
+        [Test]
+        public void TestAllMyRoutesAreGood()
+        {
+            var sol = new Solution(example);
+
+            var myResults = sol.EnumeratePaths2().ToList();
+            var exampleResults = visits.Split(Environment.NewLine).ToList();
+
+            foreach(var route in myResults)
+            {
+                Assert.That(exampleResults.Any(res => res == route.ToString()));
+            }
+        }
+
+        [Test]
+        public void IHaveAllRoutes()
+        {
+            var sol = new Solution(example);
+
+            var myResults = sol.EnumeratePaths2().ToList();
+            var exampleResults = visits.Split(Environment.NewLine).ToList();
+
+            List<string> missing = new List<string>();
+            foreach (var route in exampleResults)
+            {
+                if (!myResults.Any(res => res.ToString() == route)) missing.Add(route);
+            }
+
+            Assert.That(missing.Count == 0, $"missing:{Environment.NewLine} {string.Join(Environment.NewLine, missing)}");
+        }
+
+        public const string simpleCase = @"start-a
+start-b
+a-c
+c-d
+a-end
+b-end";
+
+        public const string example = @"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end";
+
+        public const string visits = @"start,A,b,A,b,A,c,A,end
+start,A,b,A,b,A,end
+start,A,b,A,b,end
+start,A,b,A,c,A,b,A,end
+start,A,b,A,c,A,b,end
+start,A,b,A,c,A,c,A,end
+start,A,b,A,c,A,end
+start,A,b,A,end
+start,A,b,d,b,A,c,A,end
+start,A,b,d,b,A,end
+start,A,b,d,b,end
+start,A,b,end
+start,A,c,A,b,A,b,A,end
+start,A,c,A,b,A,b,end
+start,A,c,A,b,A,c,A,end
+start,A,c,A,b,A,end
+start,A,c,A,b,d,b,A,end
+start,A,c,A,b,d,b,end
+start,A,c,A,b,end
+start,A,c,A,c,A,b,A,end
+start,A,c,A,c,A,b,end
+start,A,c,A,c,A,end
+start,A,c,A,end
+start,A,end
+start,b,A,b,A,c,A,end
+start,b,A,b,A,end
+start,b,A,b,end
+start,b,A,c,A,b,A,end
+start,b,A,c,A,b,end
+start,b,A,c,A,c,A,end
+start,b,A,c,A,end
+start,b,A,end
+start,b,d,b,A,c,A,end
+start,b,d,b,A,end
+start,b,d,b,end
+start,b,end";
     }
 }
