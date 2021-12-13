38ef28e38dac3a6c8103f1c197b29ab79b41cb1e
Geerten Vink <maedaros@yahoo.com>
2021-12-13 06:43:25 +0100

    day 13

--- Advent2021/Advent13/Solution.cs
+++ Advent2021/Advent13/Solution.cs
@@ -8,35 +8,115 @@ namespace Advent2021.Advent13
 {
     public class Solution : ISolution
     {
-        List<ParsedInput> modules;
+        public HashSet<Coordinate> dots = new HashSet<Coordinate>();
+        public List<Instruction> instructions = new List<Instruction>();
 
         public Solution(string input)
         {
-            var lines = Input.GetInputLines(input).ToArray();
+            var lines = Input.GetBlockLines(input).ToArray();
 
-            var inputParser = new InputParser<ParsedInput>("line");
+            for (int n = 0; n < lines[0].Length; n++)
+            {
+                var line = lines[0][n];
 
-            modules = inputParser.Parse(lines);
+                var parsed = line.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+
+                var coord = new Coordinate(parsed[0], parsed[1]);
+
+                dots.Add(coord);
+            }
+
+            for (int n = 0; n < lines[1].Length; n++)
+            {
+                var line = lines[1][n].Substring("fold along ".Length);
+
+                var parsed = line.Split('=', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                var instruction = new Instruction(parsed[0].ToCharArray()[0], long.Parse(parsed[1]));
+
+                instructions.Add(instruction);
+            }
         }
         public Solution() : this("Input.txt") { }
 
-        public class ParsedInput
+        public class Instruction
         {
-            [ComplexParserConstructor]
-            public ParsedInput()
-            {
+            public char axis;
+            public long index;
 
+            [ComplexParserConstructor]
+            public Instruction(char axis, long index)
+            {
+                this.axis = axis;
+                this.index = index;
             }
+
+            public HashSet<Coordinate> Fold(HashSet<Coordinate> dots)
+            {
+                HashSet<Coordinate> newCoords = new HashSet<Coordinate>();
+                foreach(var dot in dots)
+                {
+                    Coordinate newCoord = null;
+                    if (axis == 'x')
+                    {
+                        var higher = dot.X - index > 0;
+                        var distance = Math.Abs(dot.X - index);
+
+                        if (higher) newCoord = new Coordinate(dot.X - 2 * distance, dot.Y);
+                        else newCoord = new Coordinate(dot.X, dot.Y);
+                    }
+                    if (axis == 'y')
+                    {
+                        var higher = dot.Y - index > 0;
+                        var distance = Math.Abs(dot.Y - index);
+
+                        if (higher) newCoord = new Coordinate(dot.X, dot.Y - 2 * distance);
+                        else newCoord = new Coordinate(dot.X, dot.Y);
+                    }
+                    if (!newCoords.Contains(newCoord)) newCoords.Add(newCoord);
+                }
+
+                return newCoords;
+            }
+        }
+
+        public string Display(HashSet<Coordinate> coordinates)
+        {
+            var maxX = coordinates.Max(c => c.X);
+            var maxY = coordinates.Max(c => c.Y);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine();
+            for (int y = 0; y <= maxY; y++)
+            {
+                for (int x = 0; x <= maxX; x++)
+                {
+                    if (coordinates.Contains(new Coordinate(x, y))) sb.Append(Helper.BLOCK);
+                    else sb.Append(' ');
+                }
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
         }
 
         public object GetResult1()
         {
-            return "";
+            var newDots = instructions.First().Fold(dots);
+
+            return newDots.Count;
         }
 
         public object GetResult2()
         {
-            return "";
+            var newDots = dots;
+            foreach(var i in instructions)
+            {
+                newDots = i.Fold(newDots);
+            }
+
+            var result = Display(newDots);
+            return result;
         }
     }
 }
--- Advent2021/Advent13/Tests.cs
+++ Advent2021/Advent13/Tests.cs
@@ -9,7 +9,7 @@ namespace Advent2021.Advent13
 {
     class Tests
     {
-        [TestCase(example, "")]
+        [TestCase(example, 17)]
         public void Test1(string input, object output)
         {
             var sol = new Solution(input);
@@ -17,7 +17,7 @@ namespace Advent2021.Advent13
             Assert.AreEqual(output, sol.GetResult1());
         }
 
-        [TestCase(example, "")]
+        [TestCase(example, result)]
         public void Test2(string input, object output)
         {
             var sol = new Solution(input);
@@ -25,6 +25,34 @@ namespace Advent2021.Advent13
             Assert.AreEqual(output, sol.GetResult2());
         }
 
-        public const string example = @"";
+        public const string result = @"
+█████
+█   █
+█   █
+█   █
+█████
+";
+
+        public const string example = @"6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5";
     }
 }
