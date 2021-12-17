172cf50019a1b25c6fcf0f7282a4345a111d979c
Geerten Vink <maedaros@yahoo.com>
2021-12-17 10:55:42 +0100

    baarlijke nonsens

--- Advent2021/Advent17/Solution.cs
+++ Advent2021/Advent17/Solution.cs
@@ -43,30 +43,57 @@ namespace Advent2021.Advent17
 
             public Coordinate CalculatePosition(long xVelocity, long yVelocity, long turns)
             {
+                var newX = CalculateXPosition(xVelocity, turns);
+                var newY = CalculateYPosition(yVelocity, turns);
 
+                return new Coordinate(newX, newY);
+            }
+
+            public long CalculateXPosition(long xVelocity, long turns)
+            {
                 var tri = triangle(turns - 1);
 
                 long newX;
                 if (turns > xVelocity)
                 {
-                    newX = xVelocity * xVelocity - triangle(xVelocity - 1);                    
+                    newX = xVelocity * xVelocity - triangle(xVelocity - 1);
                 }
                 else newX = xVelocity * turns - tri;
 
-                var newY = yVelocity * turns - tri;
+                return newX;
+            }
 
-                return new Coordinate(newX, newY);
+            public long CalculateYPosition(long yVelocity, long turns)
+            {
+                var tri = triangle(turns - 1);
+
+                return yVelocity * turns - tri;
             }
 
             public int ValidatePosition(long xVelocity, long yVelocity, long turns)
             {
-                var position = CalculatePosition(xVelocity, yVelocity, turns);
+                var validateX = ValidateXPosition(xVelocity, turns);
 
-                if (position.X < xMin) return -1; 
-                if (position.X > xMax) return 1; 
-                if (position.Y < yMin) return 1; 
-                if (position.Y > yMax) return -1;
+                if (validateX != 0) return validateX;
+                else return ValidateYPosition(yVelocity, turns);
+            }
+
+            public int ValidateYPosition(long yVelocity, long turns)
+            {
+                var position = CalculateYPosition(yVelocity, turns);
+
+                if (position < yMin) return 1; 
+                if (position > yMax) return -1;
                 else return 0;
+            }
+
+            public int ValidateXPosition(long xVelocity, long turns)
+            {
+                var position = CalculateXPosition(xVelocity, turns);
+
+                if (position < xMin) return -1;
+                if (position > xMax) return 1;
+                return 0;
             }
 
             public bool IsValidShot(long xVelocity, long yVelocity)
@@ -89,6 +116,132 @@ namespace Advent2021.Advent17
                 return false;
             }
 
+            public List<long> TurnsForYVelocity(long yVelocity)
+            {
+                long turns = 0;
+                var stepSize = (long)Math.Pow(2.0d, 31.0d);
+                var stepUp = true;
+
+                long hitTurn = -1;
+                while (stepSize > 0)
+                {
+                    if (stepUp) turns = turns + stepSize;
+                    else turns = turns - stepSize;
+                    stepSize = stepSize / 2;
+
+                    var validation = ValidateYPosition(yVelocity, turns);
+                    if (validation == 0)
+                    {
+                        hitTurn = turns;
+                        stepSize = 0;
+                    }
+                    stepUp = (validation < 1);
+                }
+
+                var hits = new List<long>();
+                if (hitTurn != -1) hits.Add(hitTurn);
+
+                for (long runBackTurn = hitTurn - 1; ValidateYPosition(yVelocity, runBackTurn) == 0 && runBackTurn > 0; runBackTurn--)
+                {
+                    hits.Add(runBackTurn);
+                }
+                for (long runForwardTurn = hitTurn + 1; ValidateYPosition(yVelocity, runForwardTurn) == 0; runForwardTurn++)
+                {
+                    hits.Add(runForwardTurn);
+                }
+
+                return hits;
+            }
+
+            public List<long> TurnsForXVelocity(long xVelocity)
+            {
+                long turns = 0;
+                var stepSize = (long)Math.Pow(2.0d, 10.0d);
+                var stepUp = true;
+
+                long hitTurn = -1;
+                while (stepSize > 0)
+                {
+                    if (stepUp) turns = turns + stepSize;
+                    else turns = turns - stepSize;
+                    stepSize = stepSize / 2;
+
+                    var validation = ValidateXPosition(xVelocity, turns);
+                    if (validation == 0)
+                    {
+                        hitTurn = turns;
+                        stepSize = 0;
+                    }
+                    stepUp = (validation < 1);
+                }
+
+                var hits = new List<long>();
+                if (hitTurn != -1) hits.Add(hitTurn);
+
+                for (long runBackTurn = hitTurn - 1; ValidateXPosition(xVelocity, runBackTurn) == 0 && runBackTurn > 0; runBackTurn--)
+                {
+                    hits.Add(runBackTurn);
+                }
+                for (long runForwardTurn = hitTurn + 1; ValidateXPosition(xVelocity, runForwardTurn) == 0 && runForwardTurn < xMax; runForwardTurn++)
+                {
+                    hits.Add(runForwardTurn);
+                }
+
+                return hits;
+            }
+
+            public List<(long xVelocity, long yVelocity, long turn)> EnumeratePossibleHits()
+            {
+                if (PossibleHits != null) return PossibleHits;
+
+                var xHitsAtTurn = new Dictionary<long, List<long>>();
+                for (long xVelocity = 0; xVelocity <= xMax; xVelocity++)
+                {
+                    var hitTurns = TurnsForXVelocity(xVelocity);
+                    foreach (var hitTurn in hitTurns)
+                    {
+                        if (!xHitsAtTurn.ContainsKey(hitTurn)) xHitsAtTurn.Add(hitTurn, new List<long>());
+                        xHitsAtTurn[hitTurn].Add(xVelocity);
+                    }
+                }
+
+                var yHitsAtTurn = new Dictionary<long, List<long>>();
+                for (long yVelocity = yMin; yVelocity <= xMax; yVelocity++)
+                {
+                    var hitTurns = TurnsForYVelocity(yVelocity);
+                    foreach (var hitTurn in hitTurns)
+                    {
+                        if (!yHitsAtTurn.ContainsKey(hitTurn)) yHitsAtTurn.Add(hitTurn, new List<long>());
+                        yHitsAtTurn[hitTurn].Add(yVelocity);
+                    }
+                }
+
+                var result = new List<(long xVelocity, long yVelocity, long turn)>();
+
+                foreach(var kvp in yHitsAtTurn)
+                {
+                    var turn = kvp.Key;
+                    var yVelocities = kvp.Value;
+
+                    if (xHitsAtTurn.TryGetValue(turn, out List<long> xVelocities))
+                    {
+                        for (int x = 0; x < xVelocities.Count; x++)
+                        {
+                            for (int y = 0; y < yVelocities.Count; y++)
+                            {
+                                result.Add((xVelocities[x], yVelocities[y], turn));
+                            }
+                        }
+                    }
+                }
+
+                PossibleHits = result;
+
+                return result;
+            }
+
+            public List<(long xVelocity, long yVelocity, long turn)> PossibleHits { get; private set; }
+
             public long getMaxY(long xVelocity, long yVelocity)
             {
                 long turns = yVelocity;
@@ -96,43 +249,22 @@ namespace Advent2021.Advent17
 
                 return CalculatePosition(xVelocity, yVelocity, turns).Y;
             }
-
             public long triangle(long input) => input * (input + 1) / 2;
         }
 
         public object GetResult1()
         {
-            long highest = 0;
-            for (int x = 0; x < 1000; x++)
-            {
-                for (int y = 0; y < 1000; y++)
-                {
-                    if (target.IsValidShot(x, y))
-                    {
-                        var maxY = target.getMaxY(x, y);
-                        if (maxY > highest) highest = maxY;
-                    }
-                }
-            }
-
-            return highest;
+            var highestYVel = target.EnumeratePossibleHits().OrderBy(p => p.yVelocity).Last();
+            
+            return target.getMaxY(highestYVel.xVelocity, highestYVel.yVelocity);
         }
 
         public object GetResult2()
         {
-            long num = 0;
-            for (int x = 0; x < 1000; x++)
-            {
-                for (int y = -1000; y < 1000; y++)
-                {
-                    if (target.IsValidShot(x, y))
-                    {
-                        num++;
-                    }
-                }
-            }
-
-            return num;
+            return target.EnumeratePossibleHits()
+                .Select(p => (p.xVelocity, p.yVelocity))
+                .Distinct()
+                .Count();
         }
     }
 }
--- Advent2021/Program.cs
+++ Advent2021/Program.cs
@@ -11,7 +11,7 @@ namespace Advent2021
         {
             var stopWatch = new Stopwatch();
 
-            int numRuns = 1;
+            int numRuns = 10;
             string result1 = "";
             string result2 = "";
 
