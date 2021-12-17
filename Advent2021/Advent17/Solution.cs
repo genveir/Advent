fc4dc81b05d394a481ebb8aa60f65f429f65b7ed
Geerten Vink <maedaros@yahoo.com>
2021-12-17 07:10:14 +0100

    quite pretty

--- Advent2021/Advent17/Solution.cs
+++ Advent2021/Advent17/Solution.cs
@@ -8,35 +8,131 @@ namespace Advent2021.Advent17
 {
     public class Solution : ISolution
     {
-        List<ParsedInput> modules;
+        public TargetArea target;
 
         public Solution(string input)
         {
-            var lines = Input.GetInputLines(input).ToArray();
+            var line = Input.GetInputLines(input).ToArray().Single();
 
-            var inputParser = new InputParser<ParsedInput>("line");
+            var split = line.Split(new char[] { ' ', '=', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
 
-            modules = inputParser.Parse(lines);
+            var minX = long.Parse(split[3]);
+            var maxX = long.Parse(split[4]);
+            var minY = long.Parse(split[6]);
+            var maxY = long.Parse(split[7]);
+
+            target = new TargetArea(minX, maxX, minY, maxY);
         }
         public Solution() : this("Input.txt") { }
 
-        public class ParsedInput
+        public class TargetArea
         {
+            public long xMin;
+            public long xMax;
+            public long yMin;
+            public long yMax;
+
             [ComplexParserConstructor]
-            public ParsedInput()
+            public TargetArea(long xMin, long xMax, long yMin, long yMax)
+            {
+                this.xMin = xMin;
+                this.xMax = xMax;
+                this.yMin = yMin;
+                this.yMax = yMax;
+            }
+
+            public Coordinate CalculatePosition(long xVelocity, long yVelocity, long turns)
             {
 
+                var tri = triangle(turns - 1);
+
+                long newX;
+                if (turns > xVelocity)
+                {
+                    newX = xVelocity * xVelocity - triangle(xVelocity - 1);                    
+                }
+                else newX = xVelocity * turns - tri;
+
+                var newY = yVelocity * turns - tri;
+
+                return new Coordinate(newX, newY);
             }
+
+            public int ValidatePosition(long xVelocity, long yVelocity, long turns)
+            {
+                var position = CalculatePosition(xVelocity, yVelocity, turns);
+
+                if (position.X < xMin) return -1; 
+                if (position.X > xMax) return 1; 
+                if (position.Y < yMin) return 1; 
+                if (position.Y > yMax) return -1;
+                else return 0;
+            }
+
+            public bool IsValidShot(long xVelocity, long yVelocity)
+            {
+                var turns = 0;
+                var stepSize = 256;
+                var stepUp = true;
+                
+                while(stepSize > 0)
+                {
+                    if (stepUp) turns = turns + stepSize;
+                    else turns = turns - stepSize;
+                    stepSize = stepSize / 2;
+
+                    var validation = ValidatePosition(xVelocity, yVelocity, turns);
+                    if (validation == 0) return true;
+                    stepUp = (validation < 1);
+                }
+
+                return false;
+            }
+
+            public long getMaxY(long xVelocity, long yVelocity)
+            {
+                long turns = yVelocity;
+                if (yVelocity < 0) turns = 0;
+
+                return CalculatePosition(xVelocity, yVelocity, turns).Y;
+            }
+
+            public long triangle(long input) => input * (input + 1) / 2;
         }
 
         public object GetResult1()
         {
-            return "";
+            long highest = 0;
+            for (int x = 0; x < 1000; x++)
+            {
+                for (int y = 0; y < 1000; y++)
+                {
+                    if (target.IsValidShot(x, y))
+                    {
+                        var maxY = target.getMaxY(x, y);
+                        if (maxY > highest) highest = maxY;
+                    }
+                }
+            }
+
+            return highest;
         }
 
         public object GetResult2()
         {
-            return "";
+            long num = 0;
+            for (int x = 0; x < 1000; x++)
+            {
+                for (int y = -1000; y < 1000; y++)
+                {
+                    if (target.IsValidShot(x, y))
+                    {
+                        num++;
+                    }
+                }
+            }
+
+            return num;
         }
     }
 }
--- Advent2021/Advent17/Tests.cs
+++ Advent2021/Advent17/Tests.cs
@@ -25,6 +25,80 @@ namespace Advent2021.Advent17
             Assert.AreEqual(output, sol.GetResult2());
         }
 
-        public const string example = @"";
+        [TestCase(7, 2, 0, 0, 0)]
+        [TestCase(7, 2, 1, 7, 2)]
+        [TestCase(7, 2, 2, 13, 3)]
+        [TestCase(7, 2, 3, 18, 3)]
+
+        [TestCase(6, 3, 0, 0, 0)]
+        [TestCase(6, 3, 1, 6, 3)]
+        [TestCase(6, 3, 2, 11, 5)]
+        [TestCase(6, 3, 3, 15, 6)]
+        [TestCase(6, 3, 4, 18, 6)]
+        [TestCase(6, 3, 5, 20, 5)]
+        [TestCase(6, 3, 6, 21, 3)]
+        [TestCase(6, 3, 7, 21, 0)]
+        [TestCase(6, 3, 8, 21, -4)]
+        [TestCase(6, 3, 9, 21, -9)]
+        public void CalculationWorks(int xVelocity, int yVelocity, int turns, int x, int y)
+        {
+            var sol = new Solution(example);
+
+            var pos = sol.target.CalculatePosition(xVelocity, yVelocity, turns);
+
+            Assert.AreEqual(new Coordinate(x, y), pos);
+        }
+
+        [TestCase(7, 2, 0, -1)]
+        [TestCase(7, 2, 1, -1)]
+        [TestCase(7, 2, 2, -1)]
+        [TestCase(7, 2, 3, -1)]
+        [TestCase(7, 2, 4, -1)]
+        [TestCase(7, 2, 5, -1)]
+        [TestCase(7, 2, 6, -1)]
+        [TestCase(7, 2, 7, 0)]
+        [TestCase(7, 2, 8, 1)]
+        [TestCase(7, 2, 9, 1)]
+        [TestCase(7, 2, 10, 1)]
+
+        [TestCase(6, 3, 8, -1)]
+        [TestCase(6, 3, 9, 0)]
+        [TestCase(6, 3, 10, 1)]
+        public void DetectionWorks(int xVelocity, int yVelocity, int turns, long expected)
+        {
+            var sol = new Solution(example);
+
+            var pos = sol.target.ValidatePosition(xVelocity, yVelocity, turns);
+
+            Assert.AreEqual(expected, pos);
+        }
+
+        [TestCase(7, 2, true)]
+        [TestCase(6, 3, true)]
+        [TestCase(9, 0, true)]
+        [TestCase(17, -4, false)]
+        public void CanDetectHit(int xVelocity, int yVelocity, bool expected)
+        {
+            var sol = new Solution(example);
+
+            var hit = sol.target.IsValidShot(xVelocity, yVelocity);
+
+            Assert.AreEqual(expected, hit);
+        }
+
+        [TestCase(7, 2, 3)]
+        [TestCase(6, 3, 6)]
+        [TestCase(9, 0, 0)]
+        [TestCase(17, -4, 0)]
+        public void CanGetHighestY(int xVelocity, int yVelocity, int expected)
+        {
+            var sol = new Solution(example);
+
+            var maxY = sol.target.getMaxY(xVelocity, yVelocity);
+
+            Assert.AreEqual(expected, maxY);
+        }
+
+        public const string example = @"target area: x=20..30, y=-10..-5";
     }
 }
