c748c002fb2267b5c02cb6968581015efe266ca2
Geerten Vink <maedaros@yahoo.com>
2021-12-22 09:05:58 +0100

    day 22, doesn't work

--- Advent2021/Advent22/Solution.cs
+++ Advent2021/Advent22/Solution.cs
@@ -8,35 +8,237 @@ namespace Advent2021.Advent22
 {
     public class Solution : ISolution
     {
-        public List<ParsedInput> modules;
+        public List<Cube> cubes;
 
         public Solution(string input)
         {
             var lines = Input.GetInputLines(input).ToArray();
 
-            var inputParser = new InputParser<ParsedInput>("line");
+            cubes = new List<Cube>();
+            for (int n = 0; n < lines.Length; n++)
+            {
+                var line = lines[n];
 
-            modules = inputParser.Parse(lines);
+                var perCoord = line.Split(new char[] { ' ', 'x', 'y', 'z', '=', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                var on = perCoord[0] == "on";
+
+                cubes.Add(new Cube(on, 
+                    long.Parse(perCoord[1]),
+                    long.Parse(perCoord[2]),
+                    long.Parse(perCoord[3]),
+                    long.Parse(perCoord[4]),
+                    long.Parse(perCoord[5]),
+                    long.Parse(perCoord[6])));
+            }
+
+            ;
         }
         public Solution() : this("Input.txt") { }
 
-        public class ParsedInput
+        public class Cube
         {
-            [ComplexParserConstructor]
-            public ParsedInput()
-            {
+            public bool on;
+            public bool off => !on;
 
+            public long minX;
+            public long maxX;
+            public long minY;
+            public long maxY;
+            public long minZ;
+            public long maxZ;
+
+            public bool isInStartArea;
+
+            public Cube(bool on, long minX, long maxX, long minY, long maxY, long minZ, long maxZ)
+            {
+                this.on = on;
+                this.minX = Math.Min(minX, maxX);
+                this.maxX = Math.Max(minX, maxX);
+                this.minY = Math.Min(minY, maxY);
+                this.maxY = Math.Max(minY, maxY);
+                this.minZ = Math.Min(minZ, maxZ);
+                this.maxZ = Math.Max(minZ, maxZ);
+
+                isInStartArea = (   minX >= -50 && maxX <= 50 &&
+                                    minY >= -50 && maxY <= 50 &&
+                                    minZ >= -50 && maxZ <= 50);
             }
+
+            public long Size => ((maxX - minX) + 1) * ((maxY - minY) + 1) * ((maxZ - minZ) + 1);
+
+            public bool Valid
+            {
+                get
+                {
+                    return (minX <= maxX && minY <= maxY && minZ <= maxZ);
+                }
+            }
+
+            public List<Cube> SplitOnOverlap(Cube laterInInstructions)
+            {
+                var (xSize, xCutPoints) = AxisOverlap(minX, maxX, laterInInstructions.minX, laterInInstructions.maxX);
+                var (ySize, yCutPoints) = AxisOverlap(minY, maxY, laterInInstructions.minY, laterInInstructions.maxY);
+                var (zSize, zCutPoints) = AxisOverlap(minZ, maxZ, laterInInstructions.minZ, laterInInstructions.maxZ);
+
+                List<Cube> newCubes = new List<Cube>();
+
+                var overlap = xSize * ySize * zSize;
+
+                if (overlap == 0)
+                {
+                    newCubes.Add(this);
+                }
+                else if (overlap < Size)
+                {
+                    // x cut
+                    foreach (var cutpoint in xCutPoints)
+                    {
+                        if (cutpoint.keepLower)
+                        {
+                            newCubes.Add(new Cube(on, minX, cutpoint.coordinate, minY, maxY, minZ, maxZ));
+                            minX = cutpoint.coordinate + 1;
+                        }
+                        else if (!cutpoint.dropEverything)
+                        {
+                            newCubes.Add(new Cube(on, cutpoint.coordinate, maxX, minY, maxY, minZ, maxZ));
+                            maxX = cutpoint.coordinate - 1;
+                        }
+                    }
+
+                    // y cut
+                    foreach (var cutpoint in yCutPoints)
+                    {
+                        if (cutpoint.keepLower)
+                        {
+                            newCubes.Add(new Cube(on, minX, maxX, minY, cutpoint.coordinate, minZ, maxZ));
+                            minY = cutpoint.coordinate + 1;
+                        }
+                        else if(!cutpoint.dropEverything)
+                        {
+                            newCubes.Add(new Cube(on, minX, maxX, cutpoint.coordinate, maxY, minZ, maxZ));
+                            maxY = cutpoint.coordinate - 1;
+                        }
+                    }
+
+                    // z cut
+                    foreach (var cutpoint in zCutPoints)
+                    {
+                        if (cutpoint.keepLower)
+                        {
+                            newCubes.Add(new Cube(on, minX, maxX, minY, maxY, minZ, cutpoint.coordinate));
+                            minZ = cutpoint.coordinate + 1;
+                        }
+                        else if (!cutpoint.dropEverything)
+                        {
+                            newCubes.Add(new Cube(on, minX, maxX, minY, maxY, cutpoint.coordinate, maxZ));
+                            maxZ = cutpoint.coordinate - 1;
+                        }
+                    }
+                }
+
+                return newCubes;
+            }
+
+            public bool HasOverlap(Cube cube) => Overlap(cube) > 0;
+            public long Overlap(Cube cube)
+            {
+                var (xOverlap, _) = AxisOverlap(minX, maxX, cube.minX, cube.maxX);
+                var (yOverlap, _) = AxisOverlap(minY, maxY, cube.minY, cube.maxY);
+                var (zOverlap, _) = AxisOverlap(minZ, maxZ, cube.minZ, cube.maxZ);
+
+                var overlap = xOverlap * yOverlap * zOverlap;
+
+                return overlap;
+            }
+
+            public record CutPoint
+            (
+                long coordinate,
+                bool keepLower,
+                bool dropEverything
+            );
+
+            public (long length, CutPoint[] overlapEdges) AxisOverlap(long myMin, long myMax, long hisMin, long hisMax)
+            {
+                var hisSize = hisMax - hisMin + 1;
+
+                var lowCut = new CutPoint(hisMin - 1, true, false);
+                var highCut = new CutPoint(hisMax + 1, false, false);
+
+                if (hisMin <= myMin && hisMax >= myMax)
+                {
+                    return (myMax - myMin + 1, new[] { new CutPoint(0, false, true) });
+                }
+                else if (hisMin >= myMin && hisMin <= myMax)
+                {
+                    if (hisSize < myMax - hisMin + 1) return (hisSize, new[] { lowCut, highCut });
+                    return (myMax - hisMin + 1, new[] { lowCut });
+                }
+                else if (hisMax >= myMin && hisMax <= myMax)
+                {
+                    return (hisMax - myMin + 1, new[] { highCut });
+                }
+                return (0, Array.Empty<CutPoint>());
+            }
+
+            public override string ToString()
+            {
+                string onOff = on ? "on" : "off";
+                return $"{onOff}: x {minX}-{maxX}, y {minY}-{maxY}, z {minZ}-{maxZ}";
+            }
+        }
+
+        public long GetCubesThatAreOn(IEnumerable<Cube> input)
+        {
+            var cubes = input.ToList();
+
+            var distinctCubes = new List<Cube> { cubes[0] };
+
+            for (int n = 1; n < cubes.Count; n++)
+            {
+                var cube = cubes[n];
+
+                var newSplit = new List<Cube>();
+                for (int i = 0; i < distinctCubes.Count; i++)
+                { 
+                    var distinctCube = distinctCubes[i];
+
+                    var split = distinctCube.SplitOnOverlap(cube);
+
+                    foreach (var s in split) newSplit.Add(s);
+                }
+                newSplit.Add(cube);
+                distinctCubes = newSplit;
+            }
+
+            return CalculateOn(distinctCubes);
+        }
+
+        public long CalculateOn(List<Cube> distinctCubes)
+        {
+            long totalCount = 0;
+            for (int n = 0; n < distinctCubes.Count; n++)
+            {
+                var cube = distinctCubes[n];
+
+                if (cube.on) totalCount += cube.Size;
+            }
+
+            return totalCount;
         }
 
         public object GetResult1()
         {
-            return "";
+            var cubesInStartingArea = cubes.Where(c => c.isInStartArea);
+
+            return GetCubesThatAreOn(cubesInStartingArea);
+
+            
         }
 
         public object GetResult2()
         {
-            return "";
+            return "";// GetCubesThatAreOn(cubes);
         }
     }
 }
--- Advent2021/Advent22/Tests.cs
+++ Advent2021/Advent22/Tests.cs
@@ -9,7 +9,10 @@ namespace Advent2021.Advent22
 {
     class Tests
     {
-        [TestCase(example, "")]
+        [TestCase(XOnly, 1)]
+        [TestCase(XAndY, 5)]
+        [TestCase(smallExample, 39)]
+        [TestCase(example, 590784)]
         public void Test1(string input, object output)
         {
             var sol = new Solution(input);
@@ -25,6 +28,107 @@ namespace Advent2021.Advent22
             Assert.AreEqual(output, sol.GetResult2());
         }
 
-        public const string example = @"";
+        public const string XOnly = @"on x=10..12,y=1..1,z=1..1
+off x=11..13,y=1..1,z=1..1";
+
+        [Test]
+        public void CanDoSimpleSplit()
+        {
+            var sol = new Solution(XOnly);
+
+            var output = sol.cubes[0].SplitOnOverlap(sol.cubes[1]).Single();
+
+            // remainder without the overlap
+            Assert.AreEqual(10, output.minX);
+            Assert.AreEqual(10, output.maxX);
+            Assert.AreEqual(true, output.on);
+
+            // unaltered 1
+            Assert.AreEqual(11, sol.cubes[1].minX);
+            Assert.AreEqual(13, sol.cubes[1].maxX);
+            Assert.AreEqual(false, sol.cubes[1].on);
+        }
+
+        [Test]
+        public void CanDoSimpleTheOtherWay()
+        {
+            var sol = new Solution(XOnly);
+
+            var output = sol.cubes[1].SplitOnOverlap(sol.cubes[0]).Single();
+
+            // remainder without the overlap
+            Assert.AreEqual(13, output.minX);
+            Assert.AreEqual(13, output.maxX);
+            Assert.AreEqual(false, output.on);
+
+            // unaltered 1
+            Assert.AreEqual(10, sol.cubes[0].minX);
+            Assert.AreEqual(12, sol.cubes[0].maxX);
+            Assert.AreEqual(true, sol.cubes[0].on);
+        }
+
+        public const string XAndY = @"on x=10..12,y=10..12,z=1..1
+off x=11..13,y=11..13,z=1..1";
+
+        [Test]
+        public void CanDoPlaneSplit()
+        {
+            var sol = new Solution(XAndY);
+
+            var output = sol.cubes[0].SplitOnOverlap(sol.cubes[1]);
+
+            Assert.AreEqual(2, output.Count);
+
+            // bar with x-remainder
+            var xRemainder = output[0];
+            Assert.AreEqual(10, xRemainder.minX);
+            Assert.AreEqual(10, xRemainder.maxX);
+            Assert.AreEqual(10, xRemainder.minY);
+            Assert.AreEqual(12, xRemainder.maxY);
+            Assert.AreEqual(true, xRemainder.on);
+
+            // bar with y-remainder
+            var yRemainder = output[1];
+            Assert.AreEqual(11, yRemainder.minX);
+            Assert.AreEqual(12, yRemainder.maxX);
+            Assert.AreEqual(10, yRemainder.minY);
+            Assert.AreEqual(10, yRemainder.maxY);
+            Assert.AreEqual(true, yRemainder.on);
+
+            // unaltered 1
+            Assert.AreEqual(11, sol.cubes[1].minX);
+            Assert.AreEqual(13, sol.cubes[1].maxX);
+            Assert.AreEqual(11, sol.cubes[1].minY);
+            Assert.AreEqual(13, sol.cubes[1].maxY);
+            Assert.AreEqual(false, sol.cubes[1].on);
+        }
+
+        public const string example = @"on x=-20..26,y=-36..17,z=-47..7
+on x=-20..33,y=-21..23,z=-26..28
+on x=-22..28,y=-29..23,z=-38..16
+on x=-46..7,y=-6..46,z=-50..-1
+on x=-49..1,y=-3..46,z=-24..28
+on x=2..47,y=-22..22,z=-23..27
+on x=-27..23,y=-28..26,z=-21..29
+on x=-39..5,y=-6..47,z=-3..44
+on x=-30..21,y=-8..43,z=-13..34
+on x=-22..26,y=-27..20,z=-29..19
+off x=-48..-32,y=26..41,z=-47..-37
+on x=-12..35,y=6..50,z=-50..-2
+off x=-48..-32,y=-32..-16,z=-15..-5
+on x=-18..26,y=-33..15,z=-7..46
+off x=-40..-22,y=-38..-28,z=23..41
+on x=-16..35,y=-41..10,z=-47..6
+off x=-32..-23,y=11..30,z=-14..3
+on x=-49..-5,y=-3..45,z=-29..18
+off x=18..30,y=-20..-8,z=-3..13
+on x=-41..9,y=-7..43,z=-33..15
+on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
+on x=967..23432,y=45373..81175,z=27513..53682";
+
+        public const string smallExample = @"on x=10..12,y=10..12,z=10..12
+on x=11..13,y=11..13,z=11..13
+off x=9..11,y=9..11,z=9..11
+on x=10..10,y=10..10,z=10..10";
     }
 }
