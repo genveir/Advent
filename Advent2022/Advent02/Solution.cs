ea5dffe14a882430e0d71ddceb535b13c4dc8da1
Geerten Vink <maedaros@yahoo.com>
2022-12-01 22:10:25 +0100

    day 2 prep

---
+++ Advent2022/Advent02/Solution.cs
@@ -0,0 +1,42 @@
+﻿using Advent2022.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2022.Advent02
+{
+    public class Solution : ISolution
+    {
+        public List<ParsedInput> modules;
+
+        public Solution(string input)
+        {
+            var lines = Input.GetInputLines(input).ToArray();
+
+            var inputParser = new InputParser<ParsedInput>("line");
+
+            modules = inputParser.Parse(lines);
+        }
+        public Solution() : this("Input.txt") { }
+
+        public class ParsedInput
+        {
+            [ComplexParserConstructor]
+            public ParsedInput()
+            {
+
+            }
+        }
+
+        public object GetResult1()
+        {
+            return "";
+        }
+
+        public object GetResult2()
+        {
+            return "";
+        }
+    }
+}
--- Advent2022/Program.cs
+++ Advent2022/Program.cs
@@ -18,7 +18,7 @@ namespace Advent2022
             stopWatch.Start();
             for (int n = 0; n < numRuns; n++)
             {
-                ISolution solution = new Advent01.Solution();
+                ISolution solution = new Advent02.Solution();
 
                 result1 = solution.GetResult1().ToString();
                 result2 = solution.GetResult2().ToString();
