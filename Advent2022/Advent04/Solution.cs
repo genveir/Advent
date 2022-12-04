6e889e98dd4abcbcbf752761cb790def731a25fd
Geerten Vink <maedaros@yahoo.com>
2022-12-04 08:22:07 +0100

    day 4

---
+++ Advent2022/Advent04/Solution.cs
@@ -0,0 +1,68 @@
+﻿using Advent2022.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2022.Advent04
+{
+    public class Solution : ISolution
+    {
+        public List<Assignment> assignments;
+
+        public Solution(string input)
+        {
+            var lines = Input.GetInputLines(input).ToArray();
+
+            var inputParser = new InputParser<Assignment>("a-b,c-d");
+
+            assignments = inputParser.Parse(lines);
+        }
+        public Solution() : this("Input.txt") { }
+
+        public class Assignment
+        {
+            private int sectionStart;
+            private int sectionEnd;
+            private int section2Start;
+            private int section2End;
+
+            [ComplexParserConstructor]
+            public Assignment(int sectionStart, int sectionEnd, int section2Start, int section2End)
+            {
+                this.sectionStart = sectionStart;
+                this.sectionEnd = sectionEnd;
+                this.section2Start = section2Start;
+                this.section2End = section2End;
+            }
+
+            public bool HasRangeThatFullyCoversTheOther()
+            {
+                return FullyCoversTheOther(sectionStart, sectionEnd, section2Start, section2End) ||
+                    FullyCoversTheOther(section2Start, section2End, sectionStart, sectionEnd);
+            }
+
+            public static bool FullyCoversTheOther(int start, int end, int otherStart, int otherEnd)
+            {
+                return start <= otherStart && end >= otherEnd;
+            }
+
+            public bool HasOverlap()
+            {
+                return HasRangeThatFullyCoversTheOther() ||
+                    (sectionStart <= section2End && sectionEnd >= section2End) ||
+                    (sectionEnd >= section2Start && sectionStart <= section2Start);
+            }
+        }
+
+        public object GetResult1()
+        {
+            return assignments.Count(a => a.HasRangeThatFullyCoversTheOther());
+        }
+
+        public object GetResult2()
+        {
+            return assignments.Count(a => a.HasOverlap());
+        }
+    }
+}
---
+++ Advent2022/Advent04/Tests.cs
@@ -0,0 +1,35 @@
+﻿using Advent2022.Shared;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2022.Advent04
+{
+    class Tests
+    {
+        [TestCase(example, 2)]
+        public void Test1(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult1());
+        }
+
+        [TestCase(example, 4)]
+        public void Test2(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult2());
+        }
+
+        public const string example = @"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+    }
+}
