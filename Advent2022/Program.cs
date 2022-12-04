56caafb620dfaa70013b619692062a748c610be7
Geerten Vink <maedaros@yahoo.com>
2022-12-04 08:22:36 +0100

    small fixes to runner

--- Advent2022/Program.cs
+++ Advent2022/Program.cs
@@ -12,30 +12,48 @@ namespace Advent2022
             var stopWatch = new Stopwatch();
 
             int numRuns = 1;
-            string result1 = "";
-            string result2 = "";
+            object result1 = "";
+            object result2 = "";
 
             stopWatch.Start();
             for (int n = 0; n < numRuns; n++)
             {
                 ISolution solution = new AdventActive.Solution();
 
-                result1 = solution.GetResult1().ToString();
-                result2 = solution.GetResult2().ToString();
+                result1 = solution.GetResult1();
+                result2 = solution.GetResult2();
             }
             stopWatch.Stop();
 
-            if (!string.IsNullOrEmpty(result1)) ClipboardService.SetText(result1);
-            if (!string.IsNullOrEmpty(result2)) ClipboardService.SetText(result2);
+            var stringResult1 = result1.ToString();
+            var stringResult2 = result2.ToString();
 
-            Console.WriteLine(string.Format("Result for part 1: {0}", result1));
-            Console.WriteLine(string.Format("Result for part 2: {0}", result2));
+            if (!string.IsNullOrEmpty(stringResult1)) ClipboardService.SetText(stringResult1);
+            if (!string.IsNullOrEmpty(stringResult2)) ClipboardService.SetText(stringResult2);
+
+            Console.WriteLine($"Result for part 1: {stringResult1}");
+            Console.WriteLine($"Result for part 2: {stringResult2}");
             Console.WriteLine();
-            Console.WriteLine("Number of runs: " + numRuns);
-            Console.WriteLine("Total runtime: " + stopWatch.ElapsedMilliseconds + "ms");
-            Console.WriteLine("Average runtime: " + (stopWatch.ElapsedMilliseconds / numRuns) + "ms");
+            Console.WriteLine($"Number of runs: {numRuns}");
+            Console.WriteLine($"Total runtime: {FormatTime(stopWatch)}");
+            Console.WriteLine($"Average runtime: {FormatTime(stopWatch, numRuns)}");
 
             Console.ReadLine();
         }
+
+        private static string FormatTime(Stopwatch stopWatch, int? numRuns = 1)
+        {
+            TimeUnit timeUnit = TimeUnit.Milliseconds;
+            if (stopWatch.ElapsedMilliseconds / numRuns == 0) timeUnit = TimeUnit.Microseconds;
+
+            return timeUnit switch
+            {
+                TimeUnit.Microseconds => $"{stopWatch.ElapsedTicks / (TimeSpan.TicksPerMillisecond / 1000) / numRuns} micro-seconds",
+                TimeUnit.Milliseconds => $"{stopWatch.ElapsedMilliseconds / numRuns} ms",
+                _ => throw new NotSupportedException("invalid TimeUnit")
+            };
+        }
+
+        private enum TimeUnit { Milliseconds, Microseconds }
     }
 }
