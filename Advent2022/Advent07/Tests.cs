55719528e39e57c4dbf348f42101bf6d935cc132
Geerten Vink <maedaros@yahoo.com>
2022-12-07 06:37:19 +0100

    day 7

---
+++ Advent2022/Advent07/Tests.cs
@@ -0,0 +1,52 @@
+﻿using Advent2022.Shared;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2022.Advent07
+{
+    class Tests
+    {
+        [TestCase(example, 95437)]
+        public void Test1(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult1());
+        }
+
+        [TestCase(example, 24933642)]
+        public void Test2(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult2());
+        }
+
+        public const string example = @"$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k";
+    }
+}
