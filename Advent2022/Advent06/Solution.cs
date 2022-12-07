6042949ab0262e04d154089341de249cdf94d0ba
Geerten Vink <maedaros@yahoo.com>
2022-12-07 05:57:09 +0100

    day 6

---
+++ Advent2022/Advent06/Solution.cs
@@ -0,0 +1,67 @@
+﻿using Advent2022.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2022.Advent06
+{
+    public class Solution : ISolution
+    {
+        public string signal;
+
+        public Solution(string input)
+        {
+            var lines = Input.GetInputLines(input).ToArray();
+
+            signal = lines[0];
+        }
+        public Solution() : this("Input.txt") { }
+
+        public object GetResult1()
+        {
+            for (int n = 0; n < signal.Length; n++)
+            {
+                var hs = new HashSet<char>
+                {
+                    signal[n],
+                    signal[n + 1],
+                    signal[n + 2],
+                    signal[n + 3]
+                };
+
+                if (hs.Count == 4) return n + 4;
+            }
+
+            return "";
+        }
+
+        public object GetResult2()
+        {
+            for (int n = 0; n < signal.Length; n++)
+            {
+                var hs = new HashSet<char>
+                {
+                    signal[n],
+                    signal[n + 1],
+                    signal[n + 2],
+                    signal[n + 3],
+                    signal[n + 4],
+                    signal[n + 5],
+                    signal[n + 6],
+                    signal[n + 7],
+                    signal[n + 8],
+                    signal[n + 9],
+                    signal[n + 10],
+                    signal[n + 11],
+                    signal[n + 12],
+                    signal[n + 13]
+                };
+
+                if (hs.Count == 14) return n + 14;
+            }
+
+            return "";
+        }
+    }
+}
---
+++ Advent2022/Advent06/Tests.cs
@@ -0,0 +1,38 @@
+﻿using Advent2022.Shared;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Advent2022.Advent06
+{
+    class Tests
+    {
+        [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+        [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+        [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+        [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+        [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+        public void Test1(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult1());
+        }
+
+        [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+        [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+        [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+        [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+        [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+        public void Test2(string input, object output)
+        {
+            var sol = new Solution(input);
+
+            Assert.AreEqual(output, sol.GetResult2());
+        }
+
+        public const string example = @"";
+    }
+}
