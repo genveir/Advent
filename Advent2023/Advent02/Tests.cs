05e794c05c7b7f5fa5fe12ff921bbe274aeed9f9
Geerten Vink <maedaros@yahoo.com>
2023-12-01 11:19:54 +0100

    day 2 prep

---
+++ Advent2023/Advent02/Tests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2023.Advent02;
+
+class Tests
+{
+    [TestCase(example, "")]
+    public void Test1(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase(example, "")]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    public const string example = @"";
+}
