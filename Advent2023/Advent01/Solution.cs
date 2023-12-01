f19bfcc5ff3cfc3997233c37116492398ec36617
Geerten Vink <maedaros@yahoo.com>
2023-12-01 08:03:54 +0100

    day 1

--- Advent2023/Advent01/Solution.cs
+++ Advent2023/Advent01/Solution.cs
@@ -1,39 +1,88 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Advent2023.Shared;
 
-namespace Advent2023.Advent01
+namespace Advent2023.Advent01;
+
+public class Solution : ISolution
 {
-    public class Solution : ISolution
+    public List<ParsedInput> modules;
+
+    public Solution(string input)
     {
-        public List<ParsedInput> modules;
+        var lines = Input.GetInputLines(input).ToArray();
 
-        public Solution(string input)
+        var inputParser = new InputParser<ParsedInput>("line");
+
+        modules = inputParser.Parse(lines);
+    }
+    public Solution() : this("Input.txt") { }
+
+    public class ParsedInput
+    {
+        public int[] FirstNumber { get; private set; } = new int[] { -1, -1 };
+        public int[] LastNumber { get; private set; } = new int[2];
+
+        public int Value => 10 * FirstNumber[0] + LastNumber[0];
+        public int Value2 => 10 * FirstNumber[1] + LastNumber[1];
+
+        [ComplexParserConstructor]
+        public ParsedInput(string line)
         {
-            var lines = Input.GetInputLines(input).ToArray();
+            var padded = line + "     ";
 
-            var inputParser = new InputParser<ParsedInput>("line");
-
-            modules = inputParser.Parse(lines);
-        }
-        public Solution() : this("Input.txt") { }
-
-        public class ParsedInput
-        {
-            [ComplexParserConstructor]
-            public ParsedInput()
+            var numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            for (int n = 0; n < line.Length; n++)
             {
-
+                if (numbers.Contains(line[n]))
+                {
+                    Set(line[n] - '0', false);
+                }
+                else
+                {
+                    CheckNum(padded, n, "one", 1);
+                    CheckNum(padded, n, "two", 2);
+                    CheckNum(padded, n, "three", 3);
+                    CheckNum(padded, n, "four", 4);
+                    CheckNum(padded, n, "five", 5);
+                    CheckNum(padded, n, "six", 6);
+                    CheckNum(padded, n, "seven", 7);
+                    CheckNum(padded, n, "eight", 8);
+                    CheckNum(padded, n, "nine", 9);
+                }
             }
         }
 
-        public object GetResult1()
+        private void Set(int number, bool p2)
         {
-            return "";
+            if (FirstNumber[1] == -1)
+                FirstNumber[1] = number;
+            LastNumber[1] = number;
+
+            if (!p2)
+            {
+                if (FirstNumber[0] == -1)
+                    FirstNumber[0] = number;
+                LastNumber[0] = number;
+            }
         }
 
-        public object GetResult2()
-        {
-            return "";
+        private void CheckNum(string line, int index, string number, int value) 
+        { 
+            if (line.Substring(index, number.Length) == number)
+            {
+                Set(value, true);
+            }
         }
+    }
+
+    public object GetResult1()
+    {
+        return modules.Sum(m => m.Value);
+    }
+
+    public object GetResult2()
+    {
+        return modules.Sum(m => m.Value2);
     }
 }
--- Advent2023/Advent01/Tests.cs
+++ Advent2023/Advent01/Tests.cs
@@ -1,26 +1,36 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
 
-namespace Advent2023.Advent01
+namespace Advent2023.Advent01;
+
+class Tests
 {
-    class Tests
+    [TestCase(example, 142)]
+    public void Test1(string input, object output)
     {
-        [TestCase(example, "")]
-        public void Test1(string input, object output)
-        {
-            var sol = new Solution(input);
+        var sol = new Solution(input);
 
-            output.Should().Be(sol.GetResult1());
-        }
-
-        [TestCase(example, "")]
-        public void Test2(string input, object output)
-        {
-            var sol = new Solution(input);
-
-            output.Should().Be(sol.GetResult2());
-        }
-
-        public const string example = @"";
+        sol.GetResult1().Should().Be(output);
     }
+
+    [TestCase(example2, 281)]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    public const string example = @"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+
+    public const string example2 = @"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
 }
