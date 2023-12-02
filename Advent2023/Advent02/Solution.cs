7de5dc0f0e4d00910bf1290b459abee3b4319383
Geerten Vink <maedaros@yahoo.com>
2023-12-02 12:21:04 +0100

    day 2

--- Advent2023/Advent02/Solution.cs
+++ Advent2023/Advent02/Solution.cs
@@ -1,38 +1,93 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
 using Advent2023.Shared;
 
 namespace Advent2023.Advent02;
 
 public class Solution : ISolution
 {
-    public List<ParsedInput> modules;
+    public List<Game> games;
 
     public Solution(string input)
     {
-        var lines = Input.GetInputLines(input).ToArray();
+        var lines = Input.GetInputLines(input)
+            .Select(l => l.Substring(5))
+            .ToArray();
 
-        var inputParser = new InputParser<ParsedInput>("line");
+        var inputParser = new InputParser<Game>("game");
 
-        modules = inputParser.Parse(lines);
+        games = inputParser.Parse(lines);
     }
     public Solution() : this("Input.txt") { }
 
-    public class ParsedInput
+    public class Game
     {
-        [ComplexParserConstructor]
-        public ParsedInput()
-        {
+        public long Id { get; }
+        public List<Draw> Draws { get; }
 
+        [ComplexParserConstructor("num: draws")]
+        public Game(long id, Draw[] draws)
+        {
+            Id = id;
+            Draws = draws.ToList();
+        }
+    }
+
+    public class Draw
+    {
+        public List<DieGroup> DieGroups { get; }
+
+        [ComplexParserConstructor("dieGroups", ArrayDelimiters = new[] {';'})]
+        public Draw(DieGroup[] dieGroups)
+        {
+            DieGroups = dieGroups.ToList();
+        }
+    }
+
+    public class DieGroup
+    {
+        public string Color { get; }
+        public long Number { get; }
+
+        [ComplexParserConstructor("number color")]
+        public DieGroup(long number, string color)
+        {
+            Color = color;
+            Number = number;
         }
     }
 
     public object GetResult1()
     {
-        return "";
+        long sum = 0;
+
+        foreach(var game in games)
+        {
+            if (game.Draws.Any(d => d.DieGroups.Any(dg => dg.Color == "red" && dg.Number > 12))) continue;
+            if (game.Draws.Any(d => d.DieGroups.Any(dg => dg.Color == "green" && dg.Number > 13))) continue;
+            if (game.Draws.Any(d => d.DieGroups.Any(dg => dg.Color == "blue" && dg.Number > 14))) continue;
+            sum += game.Id;
+        }
+
+        return sum;
     }
 
     public object GetResult2()
     {
-        return "";
+        long sum = 0;
+
+        foreach(var game in games)
+        {
+            long red = game.Draws.SelectMany(d => d.DieGroups.Where(dg => dg.Color == "red")).Max(dg => dg.Number);
+            long green = game.Draws.SelectMany(d => d.DieGroups.Where(dg => dg.Color == "green")).Max(dg => dg.Number);
+            long blue = game.Draws.SelectMany(d => d.DieGroups.Where(dg => dg.Color == "blue")).Max(dg => dg.Number);
+
+            var power = red * green * blue;
+
+            sum += power;
+        }
+
+        return sum;
     }
 }
--- Advent2023/Advent02/Tests.cs
+++ Advent2023/Advent02/Tests.cs
@@ -5,7 +5,7 @@ namespace Advent2023.Advent02;
 
 class Tests
 {
-    [TestCase(example, "")]
+    [TestCase(example, 8)]
     public void Test1(string input, object output)
     {
         var sol = new Solution(input);
@@ -13,7 +13,7 @@ class Tests
         sol.GetResult1().Should().Be(output);
     }
 
-    [TestCase(example, "")]
+    [TestCase(example, 2286)]
     public void Test2(string input, object output)
     {
         var sol = new Solution(input);
@@ -21,5 +21,9 @@ class Tests
         sol.GetResult2().Should().Be(output);
     }
 
-    public const string example = @"";
+    public const string example = @"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 }
