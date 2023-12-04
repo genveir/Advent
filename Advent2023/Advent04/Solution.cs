300ecbe41b1468dae10feb8c52440576969d2e7a
Geerten Vink <maedaros@yahoo.com>
2023-12-04 09:27:57 +0100

    day 4

---
+++ Advent2023/Advent04/Solution.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Advent2023.Shared;
+
+namespace Advent2023.Advent04;
+
+public class Solution : ISolution
+{
+    public List<Card> cards;
+
+    public Solution(string input)
+    {
+        var lines = Input
+            .GetInputLines(input)
+            .Select(l => l.Substring(5))
+            .ToArray();
+
+        var inputParser = new InputParser<Card>("line");
+
+        cards = inputParser.Parse(lines);
+    }
+    public Solution() : this("Input.txt") { }
+
+    public class Card
+    {
+        public long Id { get; set; }
+        public long Amount { get; set; } = 1;
+
+        public long[] Winning { get; set; }
+        public long[] Actual { get; set; }
+
+        [ComplexParserConstructor("id: winning | actual", ArrayDelimiters = new[] {' '})]
+        public Card(long id, long[] winning, long[] actual)
+        {
+            Id = id;
+            Winning = winning;
+            Actual = actual;
+        }
+
+        private long _numWins = -1;
+        public long NumWins()
+        {
+            if (_numWins == -1)
+            {
+                _numWins = 0;
+                for (int n = 0; n < Winning.Length; n++)
+                {
+                    if (Actual.Contains(Winning[n]))
+                    {
+                        _numWins++;
+                    }
+                }
+            }
+            return _numWins;
+        }
+
+        public long Value()
+        {
+            var numWins = NumWins();
+            return numWins == 0 ? 0 : (long)Math.Pow(2, numWins - 1);
+        }
+    }
+
+    public object GetResult1()
+    {
+        return cards.Sum(c => c.Value());
+    }
+
+    public object GetResult2()
+    {
+        int upperBound = cards.Count - 1;
+        for (int n = 0; n <  cards.Count; n++)
+        {
+            var numWins = cards[n].NumWins();
+
+            for (int i = 1; i <= numWins; i++) 
+            {
+                if (n + i > upperBound) break;
+                cards[n + i].Amount += cards[n].Amount;
+            }
+        }
+
+        return cards.Sum(c => c.Amount);
+    }
+}
---
+++ Advent2023/Advent04/Tests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2023.Advent04;
+
+class Tests
+{
+    [TestCase(example, 13)]
+    public void Test1(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase(example2, 30)]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    public const string example = @"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+    public const string example2 = example;
+}
