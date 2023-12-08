8fe7ec5291936717de830714f6e9385d100e6944
Geerten Vink <maedaros@yahoo.com>
2023-12-08 06:46:42 +0100

    day8, no time to finish before vacation

--- Advent2023/AdventActive/Solution.cs
+++ Advent2023/AdventActive/Solution.cs
@@ -1,38 +1,80 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Advent2023.Shared;
 
 namespace Advent2023.AdventActive;
 
 public class Solution : ISolution
 {
-    public List<ParsedInput> modules;
+    public CircularList<char> Instructions;
+    public List<Location> locations;
+    Dictionary<string, Location> allLocations = new();
 
     public Solution(string input)
     {
         var lines = Input.GetInputLines(input).ToArray();
 
-        var inputParser = new InputParser<ParsedInput>("line");
+        Instructions = new(lines[0]);
 
-        modules = inputParser.Parse(lines);
+        var inputParser = new InputParser<Location>("line");
+        locations = inputParser.Parse(lines.Skip(2));
+
+        foreach (var location in locations)
+            allLocations.Add(location.Name, location);
+
+        foreach (var location in locations)
+            location.LinkUp(allLocations);
     }
     public Solution() : this("Input.txt") { }
 
-    public class ParsedInput
+    public class Location
     {
-        [ComplexParserConstructor("line")]
-        public ParsedInput()
-        {
+        public string Name { get; set; }
+        public string LeftName { get; set; }
+        public string RightName { get; set; }
 
+        public Location Left { get; set; }
+        public Location Right { get; set; }
+
+        [ComplexParserConstructor("loc = (left, right)")]
+        public Location(string name, string leftName, string rightName)
+        {
+            Name = name;
+            LeftName = leftName;
+            RightName = rightName;
         }
+
+        public void LinkUp(Dictionary<string, Location> locations)
+        {
+            Left = locations[LeftName];
+            Right = locations[RightName];
+        }
+
+        public Location Step(char direction) =>
+            (direction == 'L') ? Left : Right;
     }
 
     public object GetResult1()
     {
-        return "";
+        Location current = allLocations["AAA"];
+
+        int pathLength;
+        for (pathLength = 0; current.Name != "ZZZ"; pathLength++)
+        {
+            var step = Instructions[pathLength];
+            current = current.Step(step);
+        }
+        return pathLength;
     }
 
     public object GetResult2()
     {
+        List<Location> current = locations.Where(l => l.Name.EndsWith('A')).ToList();
+
+        // run each path until it cycles
+        // find lengths in cycle between Zs
+        // CRT over all paths
+
         return "";
     }
 }
