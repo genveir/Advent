1a0bea62097eb27a8fa1a33800d28768fc8a1896
Geerten Vink <maedaros@yahoo.com>
2024-12-01 06:10:06 +0100

    Day 1

---
+++ Advent2024/Day1/Solution.cs
@@ -0,0 +1,53 @@
+﻿using Advent2024.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2024.Day1;
+
+public class Solution : ISolution
+{
+    public List<long> left = [];
+    public List<long> right = [];
+
+    public Solution(string input)
+    {
+        var lines = Input.GetInputLines(input).ToArray();
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+            left.Add(long.Parse(parts[0]));
+            right.Add(long.Parse(parts[1]));
+        }
+
+        left.Sort();
+        right.Sort();
+    }
+
+    public Solution() : this("Input.txt")
+    {
+    }
+
+    public object GetResult1()
+    {
+        long num = 0;
+        for (int n = 0; n < left.Count; n++)
+        {
+            num += Math.Abs(left[n] - right[n]);
+        }
+
+        return num;
+    }
+
+    public object GetResult2()
+    {
+        long num = 0;
+        for (int n = 0; n < left.Count; n++)
+        {
+            num += right.Where(r => left[n] == r).Count() * left[n];
+        }
+
+        return num;
+    }
+}
---
+++ Advent2024/Day1/Tests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2024.Day1;
+
+internal class Tests
+{
+    [TestCase(example, 11)]
+    public void Test1(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase(example2, 31)]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    public const string example = @"3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+
+    public const string example2 = example;
+}
