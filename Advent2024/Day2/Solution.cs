5d259fe59b66495e45b5e04682b789fbe70a185a
Geerten Vink <maedaros@yahoo.com>
2024-12-02 06:30:43 +0100

    Day2

---
+++ Advent2024/Day2/Solution.cs
@@ -0,0 +1,130 @@
+﻿using Advent2024.Shared;
+using Advent2024.Shared.InputParsing;
+
+namespace Advent2024.Day2;
+
+public class Solution : ISolution
+{
+    public List<ParsedInput> modules;
+
+    public Solution(string input)
+    {
+        var lines = Input.GetInputLines(input).ToArray();
+
+        var inputParser = new InputParser<ParsedInput>("line");
+
+        modules = inputParser.Parse(lines);
+    }
+
+    public Solution() : this("Input.txt")
+    {
+    }
+
+    public class ParsedInput
+    {
+        private readonly long[] nums;
+
+        [ComplexParserTarget("line", ArrayDelimiters = [' '])]
+        public ParsedInput(long[] nums)
+        {
+            this.nums = nums;
+        }
+
+        public bool IsValid()
+        {
+            bool increasing = false;
+
+            if (nums[1] > nums[0])
+            {
+                increasing = true;
+            }
+
+            for (int n = 1; n < nums.Length; n++)
+            {
+                if (increasing && nums[n] < nums[n - 1])
+                {
+                    return false;
+                }
+                if (!increasing && nums[n] > nums[n - 1])
+                {
+                    return false;
+                }
+
+                var diff = Math.Abs(nums[n] - nums[n - 1]);
+
+                if (diff < 1) return false;
+                if (diff > 3) return false;
+            }
+
+            return true;
+        }
+
+        public bool IsDampenerValid(bool increasing, bool skipFirst)
+        {
+            bool dampened = skipFirst; // <-- aughh
+            long current = nums[skipFirst ? 1 : 0];
+
+            for (int n = skipFirst ? 2 : 1; n < nums.Length; n++)
+            {
+                if (increasing && nums[n] < current)
+                {
+                    if (dampened == false)
+                    {
+                        dampened = true;
+                        continue;
+                    }
+                    else return false;
+                }
+                if (!increasing && nums[n] > current)
+                {
+                    if (dampened == false)
+                    {
+                        dampened = true;
+                        continue;
+                    }
+                    else return false;
+                }
+
+                var diff = Math.Abs(nums[n] - current);
+
+                if (diff < 1)
+                {
+                    if (dampened == false)
+                    {
+                        dampened = true;
+                        continue;
+                    }
+                    else return false;
+                }
+                if (diff > 3)
+                {
+                    if (dampened == false)
+                    {
+                        dampened = true;
+                        continue;
+                    }
+                    else return false;
+                }
+
+                current = nums[n];
+            }
+
+            return true;
+        }
+    }
+
+    public object GetResult1()
+    {
+        return modules.Where(m => m.IsValid()).Count();
+    }
+
+    public object GetResult2()
+    {
+        // not 545, not 549, not 551, not 643
+        return modules.Where(m =>
+            m.IsDampenerValid(true, true) ||
+            m.IsDampenerValid(true, false) ||
+            m.IsDampenerValid(false, true) ||
+            m.IsDampenerValid(false, false)).Count();
+    }
+}
---
+++ Advent2024/Day2/Tests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2024.Day2;
+
+internal class Tests
+{
+    [TestCase(example, 2)]
+    public void Test1(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase("7 6 4 2 1", 1)]
+    [TestCase("1 2 7 8 9", 0)]
+    [TestCase("9 7 6 2 1", 0)]
+    [TestCase("1 3 2 4 5", 1)]
+    [TestCase("8 6 4 4 1", 1)]
+    [TestCase("1 3 6 7 9", 1)]
+    [TestCase(example2, 4)]
+    [TestCase("9 1 2 3 4", 1)]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    public const string example = @"7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+
+    public const string example2 = example;
+}
