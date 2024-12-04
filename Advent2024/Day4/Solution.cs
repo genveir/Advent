120d7e5027d023f0f3afb2cb195329538af8b01d
Geerten Vink <maedaros@yahoo.com>
2024-12-04 06:37:39 +0100

    day 4

---
+++ Advent2024/Day4/Solution.cs
@@ -0,0 +1,115 @@
+﻿using Advent2024.Shared;
+
+namespace Advent2024.Day4;
+
+public class Solution : ISolution
+{
+    public char[][] letters;
+
+    public Solution(string input)
+    {
+        letters = Input.GetLetterGrid(input);
+    }
+
+    public Solution() : this("Input.txt")
+    {
+    }
+
+    public long FindXMASCount()
+    {
+        long numXMAS = 0;
+
+        for (int y = 0; y < letters.Length; y++)
+        {
+            for (int x = 0; x < letters[y].Length; x++)
+            {
+                numXMAS += FindXMAS(y, x);
+            }
+        }
+
+        return numXMAS;
+    }
+
+    public long FindXMAS(int y, int x)
+    {
+        long numXMAS = 0;
+
+        numXMAS += FindXMAS(y, x, -1, -1) ? 1 : 0;
+        numXMAS += FindXMAS(y, x, -1, 0) ? 1 : 0;
+        numXMAS += FindXMAS(y, x, -1, 1) ? 1 : 0;
+
+        numXMAS += FindXMAS(y, x, 0, -1) ? 1 : 0;
+        numXMAS += FindXMAS(y, x, 0, 1) ? 1 : 0;
+
+        numXMAS += FindXMAS(y, x, 1, -1) ? 1 : 0;
+        numXMAS += FindXMAS(y, x, 1, 0) ? 1 : 0;
+        numXMAS += FindXMAS(y, x, 1, 1) ? 1 : 0;
+
+        return numXMAS;
+    }
+
+    public bool FindXMAS(int y, int x, int dx, int dy)
+    {
+        return
+            CheckLetter(y, x, 'X') &&
+            CheckLetter(y + dy, x + dx, 'M') &&
+            CheckLetter(y + 2 * dy, x + 2 * dx, 'A') &&
+            CheckLetter(y + 3 * dy, x + 3 * dx, 'S');
+    }
+
+    public long FindMAS()
+    {
+        long numMAS = 0;
+
+        for (int y = 0; y < letters.Length; y++)
+        {
+            for (int x = 0; x < letters[y].Length; x++)
+            {
+                if (FindMAS(y, x))
+                {
+                    numMAS++;
+                }
+            }
+        }
+        return numMAS;
+    }
+
+    public bool FindMAS(int y, int x)
+    {
+        if (CheckLetter(y, x, 'A'))
+        {
+            return CheckMASDiag(y, x);
+        }
+
+        return false;
+    }
+
+    private bool CheckMASDiag(int y, int x)
+    {
+        return
+            (CheckLetter(y + 1, x + 1, 'M') && CheckLetter(y - 1, x - 1, 'S') ||
+            CheckLetter(y - 1, x - 1, 'M') && CheckLetter(y + 1, x + 1, 'S')) &&
+            (CheckLetter(y + 1, x - 1, 'M') && CheckLetter(y - 1, x + 1, 'S') ||
+            CheckLetter(y - 1, x + 1, 'M') && CheckLetter(y + 1, x - 1, 'S'));
+    }
+
+    public bool CheckLetter(int y, int x, char letter)
+    {
+        if (y < 0) return false;
+        if (y >= letters.Length) return false;
+        if (x < 0) return false;
+        if (x >= letters[y].Length) return false;
+
+        return letters[y][x] == letter;
+    }
+
+    public object GetResult1()
+    {
+        return FindXMASCount();
+    }
+
+    public object GetResult2()
+    {
+        return FindMAS();
+    }
+}
---
+++ Advent2024/Day4/Tests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2024.Day4;
+
+internal class Tests
+{
+    [TestCase(example, 18)]
+    [TestCase(up, 1)]
+    [TestCase(down, 1)]
+    [TestCase(left, 1)]
+    [TestCase(right, 1)]
+    [TestCase(upRight, 1)]
+    [TestCase(upLeft, 1)]
+    [TestCase(downRight, 1)]
+    [TestCase(downLeft, 1)]
+    public void Test1(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase(example2, "")]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    public const string example = @"MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+
+    public const string up = @"S
+A
+M
+X";
+
+    public const string down = @"X
+M
+A
+S";
+
+    public const string left = @"SAMX";
+    public const string right = @"XMAS";
+
+    public const string upRight = @"   S
+  A
+ M
+X";
+
+    public const string upLeft = @"S
+ A
+  M
+   X";
+
+    public const string downRight = @"X
+ M
+  A
+   S";
+
+    public const string downLeft = @"   X
+  M
+ A
+S";
+
+    public const string example2 = example;
+}
