f81af7c7f9bb4ed21c71217889792e86d1992c36
Geerten Vink <maedaros@yahoo.com>
2024-12-06 07:16:24 +0100

    day 6

---
+++ Advent2024/Day6/Solution.cs
@@ -0,0 +1,162 @@
+﻿using Advent2024.Shared;
+
+namespace Advent2024.Day6;
+
+public class Solution : ISolution
+{
+    public char[][] grid;
+    public Coordinate2D start;
+    public Coordinate2D guard;
+    public int direction = 0;
+    public Dictionary<Coordinate2D, List<int>> visited = [];
+    public HashSet<Coordinate2D> loopSpots = [];
+
+    public Solution(string input)
+    {
+        grid = Input.GetLetterGrid(input);
+
+        for (int y = 0; y < grid.Length; y++)
+        {
+            for (int x = 0; x < grid[y].Length; x++)
+            {
+                if (grid[y][x] == '^')
+                {
+                    guard = new Coordinate2D(x, y);
+                    start = new Coordinate2D(x, y);
+                }
+            }
+        }
+    }
+
+    public Solution() : this("Input.txt")
+    {
+    }
+
+    public bool Move()
+    {
+        if (!visited.TryGetValue(guard, out List<int> directions))
+        {
+            directions = [];
+            visited.Add(guard, directions);
+        }
+        if (directions.Contains(direction)) return true;
+        directions.Add(direction);
+
+        var nextPos = direction switch
+        {
+            0 => guard.ShiftY(-1),
+            1 => guard.ShiftX(1),
+            2 => guard.ShiftY(1),
+            3 => guard.ShiftX(-1),
+            _ => throw new Exception("Invalid direction")
+        };
+
+        bool turned = false;
+        if (IsInBounds(nextPos))
+        {
+            if (grid[nextPos.Y][nextPos.X] == '#')
+            {
+                turned = true;
+                direction = (direction + 1) % 4;
+            }
+            //else
+            //{
+            //    if (CheckLoop(direction))
+            //    {
+            //        loopSpots.Add(nextPos);
+            //    }
+            //}
+        }
+
+        if (!turned) guard = nextPos;
+
+        return false;
+    }
+
+    // this doesn't work for the main problem, but why not?
+    public bool CheckLoop(int loopDir)
+    {
+        loopDir = (loopDir + 1) % 4;
+
+        var pos = guard;
+
+        while (IsInBounds(pos))
+        {
+            if (grid[pos.Y][pos.X] == '#') return false;
+
+            if (visited.TryGetValue(pos, out var directions))
+            {
+                if (directions.Contains(loopDir))
+                {
+                    return true;
+                }
+            }
+
+            pos = loopDir switch
+            {
+                0 => pos.ShiftY(-1),
+                1 => pos.ShiftX(1),
+                2 => pos.ShiftY(1),
+                3 => pos.ShiftX(-1),
+                _ => throw new Exception("Invalid direction")
+            };
+        }
+
+        return false;
+    }
+
+    public bool IsInBounds(Coordinate2D pos)
+    {
+        return pos.X >= 0 && pos.X < grid[0].Length && pos.Y >= 0 && pos.Y < grid.Length;
+    }
+
+    public object GetResult1()
+    {
+        while (IsInBounds(guard))
+        {
+            Move();
+        }
+
+        return visited.Count;
+    }
+
+    // not 492, not 443
+    public object GetResult2()
+    {
+        //while (IsInBounds(guard))
+        //{
+        //    Move();
+        //}
+
+        //return loopSpots.Count;
+
+        for (int y = 0; y < grid.Length; y++)
+        {
+            for (int x = 0; x < grid[y].Length; x++)
+            {
+                var current = grid[y][x];
+
+                grid[y][x] = '#';
+
+                guard = start;
+                direction = 0;
+                visited = [];
+
+                while (IsInBounds(guard))
+                {
+                    var exitedBecauseOfLoop = Move();
+
+                    if (exitedBecauseOfLoop)
+                    {
+                        loopSpots.Add(new(x, y));
+                        break;
+                    }
+                }
+
+                grid[y][x] = current;
+            }
+        }
+
+        return loopSpots.Count;
+    }
+}
---
+++ Advent2024/Day6/Tests.cs
@@ -0,0 +1,54 @@
+﻿using Advent2024.Shared;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2024.Day6;
+
+internal class Tests
+{
+    [TestCase(example, 41)]
+    [TestCase("Input.txt", 5101)]
+    public void Test1(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase(example2, 6)]
+    [TestCase("Input.txt", 1951)]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    [TestCase("3,6", example, 3, 6)]
+    [TestCase("6,7", example, 6, 7)]
+    [TestCase("7,7", example, 7, 7)]
+    [TestCase("1,8", example, 1, 8)]
+    [TestCase("3,8", example, 3, 8)]
+    [TestCase("7,9", example, 7, 9)]
+    public void TestActualSpots(string id, string input, int x, int y)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2();
+
+        sol.loopSpots.Should().Contain(new Coordinate2D(x, y));
+    }
+
+    public const string example = @"....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+
+    public const string example2 = example;
+}
