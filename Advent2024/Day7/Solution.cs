2f0578934e8efc24b15b51c6398d98f438975f7c
Geerten Vink <maedaros@yahoo.com>
2024-12-07 06:39:10 +0100

    Day 7

---
+++ Advent2024/Day7/Solution.cs
@@ -0,0 +1,89 @@
+﻿using Advent2024.Shared;
+using Advent2024.Shared.InputParsing;
+
+namespace Advent2024.Day7;
+
+public class Solution : ISolution
+{
+    public List<ParsedInput> modules;
+
+    public Solution(string input)
+    {
+        var lines = Input.GetInputLines(input).ToArray();
+
+        var inputParser = new InputParser<ParsedInput>("line");
+
+        modules = inputParser.Parse(lines);
+    }
+
+    public Solution() : this("Input.txt")
+    {
+    }
+
+    public class ParsedInput
+    {
+        [ComplexParserTarget("testValue: values", ArrayDelimiters = [' '])]
+        public ParsedInput(long testValue, long[] values)
+        {
+            TestValue = testValue;
+            Values = values.Reverse().ToArray();
+        }
+
+        public long TestValue { get; set; }
+        public long[] Values { get; set; }
+
+        public long[] GetPossibleValues(int index, bool withConcat)
+        {
+            if (index == Values.Length - 1)
+            {
+                return [Values[index]];
+            }
+
+            var value = Values[index];
+
+            var fromHere = GetPossibleValues(index + 1, withConcat);
+
+            var concatenated = fromHere.Select(
+                fh => long.Parse(fh.ToString() + value.ToString()));
+
+            var result = fromHere
+                .Select(v => v + value)
+                .Concat(fromHere.Select(v => v * value))
+                .ToArray();
+
+            if (withConcat)
+                result = result
+                    .Concat(concatenated)
+                    .ToArray();
+
+            return result;
+        }
+
+        public bool TestValueIsPossible(bool withConcat)
+        {
+            var possibleValues = GetPossibleValues(0, withConcat);
+
+            return possibleValues.Any(v => v == TestValue);
+        }
+
+        public override string ToString()
+        {
+            return $"{TestValue}: {string.Join(' ', Values)}";
+        }
+    }
+
+    public object GetResult1()
+    {
+        return modules
+            .Where(m => m.TestValueIsPossible(false))
+            .Sum(m => m.TestValue);
+    }
+
+    // not 456567254157484
+    public object GetResult2()
+    {
+        return modules
+            .Where(m => m.TestValueIsPossible(true))
+            .Sum(m => m.TestValue);
+    }
+}
---
+++ Advent2024/Day7/Tests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2024.Day7;
+
+internal class Tests
+{
+    [TestCase(example, 3749)]
+    public void Test1(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase(example2, 11387)]
+    [TestCase("7290: 6 8 6 15", 7290)]
+    [TestCase("192: 17 8 14", 192)]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    public const string example = @"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20";
+
+    public const string example2 = example;
+}
