8c8a471f7462cf7c158e5d80d6d54b29ecf5a2b7
Geerten Vink <maedaros@yahoo.com>
2024-12-08 06:18:58 +0100

    day 8

---
+++ Advent2024/Day08/Solution.cs
@@ -0,0 +1,145 @@
+﻿using System.Text;
+
+namespace Advent2024.Day08;
+
+public class Solution
+{
+    public List<char> frequencies = [];
+    public Dictionary<char, List<Coordinate2D>> antennas = [];
+    public Dictionary<Coordinate2D, char> antennasByLoc = [];
+
+    public HashSet<Coordinate2D> antiNodes = [];
+
+    public char[][] grid;
+
+    public Solution(string input)
+    {
+        grid = Input.GetLetterGrid(input);
+
+        for (int y = 0; y < grid.Length; y++)
+        {
+            for (int x = 0; x < grid[y].Length; x++)
+            {
+                if (grid[y][x] != '.')
+                {
+                    if (!frequencies.Contains(grid[y][x]))
+                    {
+                        frequencies.Add(grid[y][x]);
+                        antennas[grid[y][x]] = [];
+                    }
+                    antennas[grid[y][x]].Add(new Coordinate2D(x, y));
+                    antennasByLoc[new Coordinate2D(x, y)] = grid[y][x];
+                }
+            }
+        }
+    }
+
+    public void CalculateAntiNodes()
+    {
+        foreach (var freq in frequencies)
+        {
+            foreach (var ant in antennas[freq])
+            {
+                foreach (var ant2 in antennas[freq])
+                {
+                    if (ant != ant2)
+                    {
+                        var diffX = ant2.X - ant.X;
+                        var diffY = ant2.Y - ant.Y;
+
+                        var node1 = new Coordinate2D(ant.X - diffX, ant.Y - diffY);
+                        var node2 = new Coordinate2D(ant2.X + diffX, ant2.Y + diffY);
+
+                        if (IsInGrid(node1)) antiNodes.Add(node1);
+                        if (IsInGrid(node2)) antiNodes.Add(node2);
+                    }
+                }
+            }
+        }
+    }
+
+    public void CalculateAntiNodes2()
+    {
+        foreach (var freq in frequencies)
+        {
+            foreach (var ant in antennas[freq])
+            {
+                foreach (var ant2 in antennas[freq])
+                {
+                    if (ant != ant2)
+                    {
+                        var diffX = ant2.X - ant.X;
+                        var diffY = ant2.Y - ant.Y;
+
+                        var posNode = new Coordinate2D(ant.X + diffX, ant.Y + diffY);
+
+                        while (IsInGrid(posNode))
+                        {
+                            antiNodes.Add(posNode);
+                            posNode = new Coordinate2D(posNode.X + diffX, posNode.Y + diffY);
+                        }
+
+                        var negNode = new Coordinate2D(ant.X - diffX, ant.Y - diffY);
+
+                        while (IsInGrid(negNode))
+                        {
+                            antiNodes.Add(negNode);
+                            negNode = new Coordinate2D(negNode.X - diffX, negNode.Y - diffY);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private bool IsInGrid(Coordinate2D coord)
+    {
+        return coord.X >= 0 && coord.X < grid[0].Length && coord.Y >= 0 && coord.Y < grid.Length;
+    }
+
+    public string PrintGrid()
+    {
+        StringBuilder builder = new();
+
+        for (int y = 0; y < grid.Length; y++)
+        {
+            for (int x = 0; x < grid[y].Length; x++)
+            {
+                if (antennasByLoc.ContainsKey(new Coordinate2D(x, y)))
+                {
+                    builder.Append(antennasByLoc[new Coordinate2D(x, y)]);
+                }
+                else if (antiNodes.Contains(new Coordinate2D(x, y)))
+                {
+                    builder.Append('#');
+                }
+                else
+                {
+                    builder.Append('.');
+                }
+            }
+            builder.AppendLine();
+        }
+        return builder.ToString().Trim();
+    }
+
+    public Solution() : this("Input.txt")
+    {
+    }
+
+
+
+    public object GetResult1()
+    {
+        CalculateAntiNodes();
+
+        return antiNodes.Count;
+    }
+
+    public object GetResult2()
+    {
+        CalculateAntiNodes2();
+
+        return antiNodes.Count;
+    }
+}
---
+++ Advent2024/Day08/Tests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2024.Day08;
+
+internal class Tests
+{
+    [TestCase(example, 14)]
+    public void Test1(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase(example2, 34)]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    [Test]
+    public void ExampleTest1()
+    {
+        var sol = new Solution(example);
+
+        sol.CalculateAntiNodes();
+
+        sol.PrintGrid().Should().Be(exampleExpected);
+    }
+
+    public const string example = @"............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............";
+
+    public const string example2 = example;
+
+    public const string exampleExpected = @"......#....#
+...#....0...
+....#0....#.
+..#....0....
+....0....#..
+.#....A.....
+...#........
+#......#....
+........A...
+.........A..
+..........#.
+..........#.";
+}
