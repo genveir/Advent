8a19bd10e7b02d7cd33dd03380ef3c2339ad9e50
Geerten Vink <maedaros@yahoo.com>
2024-12-11 06:30:25 +0100

    Day 11

---
+++ Advent2024/Day11/Solution.cs
@@ -0,0 +1,100 @@
+﻿namespace Advent2024.Day11;
+
+public class Solution
+{
+    public long[] nums;
+    public long[] rocks;
+
+    public Dictionary<long, long[]> cachedSplits = [];
+
+    public int NumBlinksPt1 { get; set; } = 25;
+
+    public Solution(string input)
+    {
+        nums = Input.GetNumbers(input, [' ']);
+
+        Reset();
+    }
+
+    public void Reset()
+    {
+        rocks = Helper.DeepCopy(nums);
+    }
+
+    public Solution() : this("Input.txt")
+    {
+    }
+
+    public long[] Split(long value, Dictionary<long, long[]> cachedSplits)
+    {
+        if (!cachedSplits.TryGetValue(value, out var splits))
+        {
+            if (value == 0)
+            {
+                splits = [1];
+            }
+            else
+            {
+                var asString = value.ToString();
+
+                if (asString.Length % 2 == 0)
+                {
+                    var half = asString.Length / 2;
+                    var left = asString[..half];
+                    var right = asString[half..];
+                    splits = [long.Parse(left), long.Parse(right)];
+                }
+                else
+                {
+                    splits = [value * 2024];
+                }
+            }
+
+            cachedSplits[value] = splits;
+        }
+        return splits;
+    }
+
+    public Dictionary<(long rock, long blinks), long> cachedCounts = [];
+
+    public long GetCountForSingleRock(long rock, long blinks)
+    {
+        if (blinks == 0) return 1;
+
+        if (!cachedCounts.TryGetValue((rock, blinks), out var count))
+        {
+            var split = Split(rock, cachedSplits);
+
+            count = split.Sum(r => GetCountForSingleRock(r, blinks - 1));
+
+            cachedCounts[(rock, blinks)] = count;
+        }
+
+        return count;
+    }
+
+    public object GetResult1()
+    {
+        Reset();
+        long sum = 0;
+
+        foreach (var rock in rocks)
+        {
+            sum += GetCountForSingleRock(rock, NumBlinksPt1);
+        }
+        return sum;
+    }
+
+    public object GetResult2()
+    {
+        Reset();
+
+        long sum = 0;
+
+        foreach (var rock in rocks)
+        {
+            sum += GetCountForSingleRock(rock, 75);
+        }
+        return sum;
+    }
+}
---
+++ Advent2024/Day11/Tests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2024.Day11;
+
+internal class Tests
+{
+    [TestCase(example, 55312)]
+    public void Test1(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase("0 1 10 99 999", 7)]
+    public void SingleBlink(string input, object output)
+    {
+        var sol = new Solution(input)
+        {
+            NumBlinksPt1 = 1
+        };
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase(example2, 65601038650482)]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    public const string example = @"125 17";
+
+    public const string example2 = example;
+}
