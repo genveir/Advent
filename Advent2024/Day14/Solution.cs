799678914cfb1ab3103ddff6bc17e1373882405d
Geerten Vink <maedaros@yahoo.com>
2024-12-14 11:59:26 +0100

    Day 14

---
+++ Advent2024/Day14/Solution.cs
@@ -0,0 +1,181 @@
+﻿namespace Advent2024.Day14;
+
+public class Solution
+{
+    public List<ParsedInput> modules;
+
+    public long Height { get; set; } = 103;
+    public long Width { get; set; } = 101;
+
+    public Solution(string input)
+    {
+        var lines = Input.GetInputLines(input).ToArray();
+
+        var inputParser = new InputParser<ParsedInput>("line");
+
+        modules = inputParser.Parse(lines);
+    }
+
+    public Solution() : this("Input.txt")
+    {
+    }
+
+    public class ParsedInput
+    {
+        public Coordinate2D position;
+        public Coordinate2D velocity;
+
+        [ComplexParserTarget("\\p=coords \\v=coords")]
+        public ParsedInput(Coordinate2D position, Coordinate2D velocity)
+        {
+            this.position = position;
+            this.velocity = velocity;
+        }
+
+        public Coordinate2D SimulateSteps(long steps, long width, long height)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                SimulateStep(width, height);
+            }
+
+            return position;
+        }
+
+        public void SimulateStep(long width, long height)
+        {
+            position = position.Shift(velocity.X, velocity.Y);
+
+            position = new Coordinate2D(ModNum(position.X, width), ModNum(position.Y, height));
+        }
+
+        public Coordinate2D PositionAt(long tick, long width, long height)
+        {
+            var xShift = velocity.X * tick;
+            var yShift = velocity.Y * tick;
+
+            var newPos = position.Shift(xShift, yShift);
+
+            var modPos = new Coordinate2D(ModNum(newPos.X, width), ModNum(newPos.Y, height));
+
+            return modPos;
+        }
+
+        public Coordinate2D ModPos(Coordinate2D pos, long width, long height)
+        {
+            return new Coordinate2D(ModNum(pos.X, width), ModNum(pos.Y, height));
+        }
+
+        public long ModNum(long num, long modulo)
+        {
+            num = num % modulo;
+            if (num < 0) num += modulo;
+            return num;
+        }
+
+    }
+
+    public class Quadrant
+    {
+        public Quadrant(Coordinate2D topLeft, Coordinate2D bottomRight)
+        {
+            TopLeft = topLeft;
+            BottomRight = bottomRight;
+        }
+
+        public Coordinate2D TopLeft { get; set; }
+        public Coordinate2D BottomRight { get; set; }
+    }
+
+    public bool IsInQuadrant(Coordinate2D position, Quadrant quadrant)
+    {
+        return position.X >= quadrant.TopLeft.X && position.X <= quadrant.BottomRight.X &&
+               position.Y >= quadrant.TopLeft.Y && position.Y <= quadrant.BottomRight.Y;
+    }
+
+    public List<Quadrant> MakeQuadrants()
+    {
+        var list = new List<Quadrant>();
+
+        var topLeftQuadrant = new Quadrant(new Coordinate2D(0, 0), new Coordinate2D(Width / 2 - 1, Height / 2 - 1));
+        var topRightQuadrant = new Quadrant(new Coordinate2D(Width / 2 + 1, 0), new Coordinate2D(Width - 1, Height / 2 - 1));
+        var bottomLeftQuadrant = new Quadrant(new Coordinate2D(0, Height / 2 + 1), new Coordinate2D(Width / 2 - 1, Height - 1));
+        var bottomRightQuadrant = new Quadrant(new Coordinate2D(Width / 2 + 1, Height / 2 + 1), new Coordinate2D(Width - 1, Height - 1));
+
+        list.Add(topLeftQuadrant);
+        list.Add(topRightQuadrant);
+        list.Add(bottomLeftQuadrant);
+        list.Add(bottomRightQuadrant);
+
+        return list;
+    }
+
+    // not 208926000 <-- this is what you get when you swap Height and Width :/
+    public object GetResult1()
+    {
+        var positions = new List<Coordinate2D>();
+
+        foreach (var module in modules)
+        {
+            positions.Add(module.PositionAt(100, Width, Height));
+        }
+
+        var quadrants = MakeQuadrants();
+
+        var topLeftQuadrant = quadrants[0];
+        var topRightQuadrant = quadrants[1];
+        var bottomLeftQuadrant = quadrants[2];
+        var bottomRightQuadrant = quadrants[3];
+
+        var topLeftCount = positions.Count(p => IsInQuadrant(p, topLeftQuadrant));
+        var topRightCount = positions.Count(p => IsInQuadrant(p, topRightQuadrant));
+        var bottomLeftCount = positions.Count(p => IsInQuadrant(p, bottomLeftQuadrant));
+        var bottomRightCount = positions.Count(p => IsInQuadrant(p, bottomRightQuadrant));
+
+        return topLeftCount * topRightCount * bottomLeftCount * bottomRightCount;
+    }
+
+    public object GetResult2()
+    {
+        int counter = 0;
+        while (true)
+        {
+            counter++;
+
+            foreach (var module in modules)
+            {
+                module.SimulateStep(Width, Height);
+            }
+
+            if (modules.Select(m => m.position).Distinct().Count() == modules.Count)
+            {
+                Console.Clear();
+                Console.WriteLine(counter);
+                Print();
+                var input = Console.ReadLine();
+
+                if (input == "y")
+                    return counter;
+            }
+        }
+    }
+
+    public void Print()
+    {
+        for (int y = 0; y < Height; y++)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                if (modules.Any(m => m.position.X == x && m.position.Y == y))
+                {
+                    Console.Write("#");
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Advent2024/Day14/Tests.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2024.Day14;
+
+internal class Tests
+{
+    [TestCase(example, 11, 7, 12)]
+    public void Test1(string input, long width, long height, object output)
+    {
+        var sol = new Solution(input);
+        sol.Width = width;
+        sol.Height = height;
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [Test]
+    public void SimulateMatchesExample()
+    {
+        var bot = new Solution.ParsedInput(new Coordinate2D(2, 4), new Coordinate2D(2, -3));
+
+        bot.SimulateSteps(1, 11, 7).Should().Be(new Coordinate2D(4, 1));
+        bot.SimulateSteps(1, 11, 7).Should().Be(new Coordinate2D(6, 5));
+        bot.SimulateSteps(1, 11, 7).Should().Be(new Coordinate2D(8, 2));
+        bot.SimulateSteps(1, 11, 7).Should().Be(new Coordinate2D(10, 6));
+    }
+
+    [TestCase(example)]
+    [TestCase("Input.txt")]
+    public void SimulateMatchesPosition(string input)
+    {
+        var sol = new Solution(input);
+        var calculated = sol.modules.Select(m => m.PositionAt(100, sol.Width, sol.Height)).ToArray();
+        var simulated = sol.modules.Select(m => m.SimulateSteps(100, sol.Width, sol.Height)).ToArray();
+
+        foreach (var (calc, sim) in calculated.Zip(simulated))
+        {
+            calc.Should().Be(sim);
+        }
+    }
+
+    [TestCase(11, 7, 4, 2)]
+    [TestCase(103, 101, 50, 49)]
+    [TestCase(3, 5, 0, 1)]
+    public void TestQuadrants(long width, long height, long wExpect, long hExpect)
+    {
+        var sol = new Solution(example);
+        sol.Width = width;
+        sol.Height = height;
+
+        var quadrants = sol.MakeQuadrants();
+
+        quadrants[0].TopLeft.Should().Be(new Coordinate2D(0, 0));
+        quadrants[0].BottomRight.Should().Be(new Coordinate2D(wExpect, hExpect));
+
+        quadrants[1].TopLeft.Should().Be(new Coordinate2D(wExpect + 2, 0));
+        quadrants[1].BottomRight.Should().Be(new Coordinate2D(width - 1, hExpect));
+
+        quadrants[2].TopLeft.Should().Be(new Coordinate2D(0, hExpect + 2));
+        quadrants[2].BottomRight.Should().Be(new Coordinate2D(wExpect, height - 1));
+
+        quadrants[3].TopLeft.Should().Be(new Coordinate2D(wExpect + 2, hExpect + 2));
+        quadrants[3].BottomRight.Should().Be(new Coordinate2D(width - 1, height - 1));
+    }
+
+    [TestCase(example2, 1)] // not a proper test
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    public const string example = @"p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3";
+
+    public const string example2 = example;
+}
