9790732ca72652f7f0568cbde2d146066942672e
Geerten Vink <maedaros@yahoo.com>
2024-12-19 06:22:43 +0100

    Day 19

---
+++ Advent2024/Day19/Solution.cs
@@ -0,0 +1,74 @@
+﻿namespace Advent2024.Day19;
+
+public class Solution
+{
+    public string[] patterns;
+    public string[] targets;
+
+    public Solution(string input)
+    {
+        var blocks = Input.GetBlockLines(input).ToArray();
+
+        patterns = blocks[0].Single().Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        targets = blocks[1];
+    }
+
+    public Solution() : this("Input.txt")
+    {
+    }
+
+    public Dictionary<string, long> matches = [];
+    public long Solve(string target)
+    {
+        if (matches.TryGetValue(target, out var result))
+        {
+            return result;
+        }
+        if (target.Length == 0)
+        {
+            return 1;
+        }
+
+        var paddedTarget = target + "           ";
+
+        long solveCount = 0;
+        foreach (var pattern in patterns)
+        {
+            if (paddedTarget.StartsWith(pattern))
+            {
+                var waysToSolveSubTarget = Solve(target[pattern.Length..]);
+
+                solveCount += waysToSolveSubTarget;
+            }
+        }
+
+        matches[target] = solveCount;
+        return solveCount;
+    }
+
+    public object GetResult1()
+    {
+        int count = 0;
+        foreach (var target in targets)
+        {
+            var result = Solve(target);
+            if (result != 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public object GetResult2()
+    {
+        long count = 0;
+        foreach (var target in targets)
+        {
+            var result = Solve(target);
+
+            count += result;
+        }
+        return count;
+    }
+}
---
+++ Advent2024/Day19/Tests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2024.Day19;
+
+internal class Tests
+{
+    [TestCase(example, 6)]
+    public void Test1(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase(example2, "")]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    public const string example = @"r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb";
+
+    public const string example2 = example;
+}
