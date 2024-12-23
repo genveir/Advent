4659626005eb61685849fe11dc29ddeb9daa0f18
Geerten Vink <maedaros@yahoo.com>
2024-12-23 17:48:39 +0100

    Day 23

---
+++ Advent2024/Day23/Solution.cs
@@ -0,0 +1,147 @@
+﻿namespace Advent2024.Day23;
+
+public class Solution
+{
+    public List<Computer> Computers { get; set; } = [];
+
+    public Solution(string input)
+    {
+        var lines = Input.GetInputLines(input).ToArray();
+
+        var inputParser = new InputParser<string, string>("comp-comp");
+        var parsed = inputParser.Parse(lines);
+
+        HashSet<string> computers = [];
+        foreach (var (comp1, comp2) in parsed)
+        {
+            computers.Add(comp1);
+            computers.Add(comp2);
+        }
+
+        foreach (var comp in computers)
+        {
+            Computers.Add(new Computer(comp));
+        }
+
+        foreach (var (comp1, comp2) in parsed)
+        {
+            var computer1 = Computers.Single(c => c.Name == comp1);
+            var computer2 = Computers.Single(c => c.Name == comp2);
+            computer1.Connect(computer2);
+        }
+    }
+    public Solution() : this("Input.txt") { }
+
+    public class Computer
+    {
+        public string Name { get; set; }
+        public HashSet<Computer> Connections { get; set; }
+
+        public Computer(string name)
+        {
+            Name = name;
+            Connections = [];
+        }
+
+        public void Connect(Computer other)
+        {
+            Connections.Add(other);
+            other.Connections.Add(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Computer {Name}";
+        }
+    }
+
+    public object GetResult1()
+    {
+        var computersWithT = Computers
+            .Where(c => c.Name.StartsWith("t"))
+            .ToArray();
+
+        long num = 0;
+        foreach (var computer in computersWithT)
+        {
+            // tb => vc
+            foreach (var connection in computer.Connections)
+            {
+                if (connection.Name.StartsWith("t") && string.Compare(computer.Name, connection.Name) < 0)
+                    continue;
+
+                // vc -> wq
+                foreach (var c2 in connection.Connections)
+                {
+                    if (c2.Name.StartsWith("t") && string.Compare(computer.Name, c2.Name) < 0)
+                        continue;
+
+                    if (c2.Connections.Contains(computer))
+                        num++;
+                }
+            }
+        }
+
+        return num / 2;
+    }
+
+    public List<Computer> FindMaximalClique()
+    {
+        HashSet<Computer> hasBeenChecked = [];
+        List<Computer> largestGroup = [];
+        foreach (var computer in Computers)
+        {
+            if (hasBeenChecked.Contains(computer))
+                continue;
+
+            List<Computer> clique = FindMaximalClique(computer);
+
+            foreach (var comp in clique)
+                hasBeenChecked.Add(comp);
+
+            if (clique.Count > largestGroup.Count)
+                largestGroup = clique;
+        }
+
+        return largestGroup;
+    }
+
+    public List<Computer> FindMaximalClique(Computer computer)
+    {
+        var connected = computer.Connections;
+
+        return FindMaximalClique([computer], connected.ToList(), []).Item2;
+    }
+
+    public (bool, List<Computer>) FindMaximalClique(List<Computer> inClique, List<Computer> candidates, List<Computer> excluded)
+    {
+        while (true)
+        {
+            if (candidates.Count == 0 && excluded.Count == 0)
+                return (true, inClique);
+
+            foreach (var vertex in candidates)
+            {
+                var newClique = inClique.ToList();
+                newClique.Add(vertex);
+
+                var newCandidates = candidates.Intersect(vertex.Connections).ToList();
+                var newExcluded = excluded.Intersect(vertex.Connections).ToList();
+
+                var (succeeded, result) = FindMaximalClique(newClique, newCandidates, newExcluded);
+                if (succeeded)
+                    return (true, result);
+
+                candidates.Remove(vertex);
+                excluded.Add(vertex);
+            }
+        }
+    }
+
+    public object GetResult2()
+    {
+        List<Computer> largestGroup = FindMaximalClique();
+
+        return string.Join(',', largestGroup.OrderBy(c => c.Name).Select(c => c.Name));
+    }
+}
---
+++ Advent2024/Day23/Tests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2024.Day23;
+
+class Tests
+{
+    [TestCase(example, 7)]
+    public void Test1(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase(example2, "co,de,ka,ta")]
+    [TestCase(simple, "a,b,c")]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    public const string example = @"kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn";
+
+    public const string simple = @"a-b
+a-c
+b-c
+c-d";
+
+    public const string example2 = example;
+}
