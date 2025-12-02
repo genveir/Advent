8553c18bf5f694429703e89a0897e95919a93e9e
Geerten Vink <maedaros@yahoo.com>
2025-12-02 09:57:29 +0100

    Day 2

---
+++ Advent2025/Day02/Solution.cs
@@ -0,0 +1,109 @@
+﻿namespace Advent2025.Day02;
+
+public class Solution
+{
+    public List<IdRange> idRanges;
+
+    public Solution(string input)
+    {
+        var lines = Input.GetInputLines(input, splitOn: ['\n', '\r', ',']);
+
+        var inputParser = new InputParser<IdRange>("line");
+
+        idRanges = inputParser.Parse(lines);
+    }
+    public Solution() : this("Input.txt") { }
+
+    public class IdRange
+    {
+        public string First { get; }
+        public string Last { get; }
+
+        [ComplexParserTarget("first-last")]
+        public IdRange(string first, string last)
+        {
+            First = first;
+            Last = last;
+        }
+
+        public IEnumerable<long> FindInvalidIds()
+        {
+            long first = long.Parse(First);
+            long last = long.Parse(Last);
+
+            for (long id = first; id <= last; id++)
+            {
+                var asString = id.ToString();
+
+                if (asString.Length % 2 != 0)
+                    continue;
+
+                if (IsSuperInvalid(asString, asString.Length / 2))
+                {
+                    yield return id;
+                }
+            }
+        }
+
+        public IEnumerable<long> FindSuperInvalidIds()
+        {
+            long first = long.Parse(First);
+            long last = long.Parse(Last);
+
+            for (long id = first; id <= last; id++)
+            {
+                var asString = id.ToString();
+                for (int n = 1; n <= asString.Length / 2; n++)
+                {
+                    if (IsSuperInvalid(asString, n))
+                    {
+                        yield return id;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private bool IsSuperInvalid(string idString, int subStringLength)
+        {
+            if (idString.Length % subStringLength != 0)
+                return false;
+
+            var subString = idString.Substring(0, subStringLength);
+            for (int pos = subStringLength; pos < idString.Length; pos += subStringLength)
+            {
+                if (idString.Substring(pos, subStringLength) != subString)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override string ToString() => $"{First}-{Last}";
+    }
+
+    // total space is only 2302978 items
+    public object GetResult1()
+    {
+        List<long> invalidIds = [];
+
+        foreach (var idRange in idRanges)
+        {
+            invalidIds.AddRange(idRange.FindInvalidIds());
+        }
+
+        return invalidIds.Sum();
+    }
+
+    public object GetResult2()
+    {
+        List<long> invalidIds = [];
+
+        foreach (var idRange in idRanges)
+        {
+            invalidIds.AddRange(idRange.FindSuperInvalidIds());
+        }
+
+        return invalidIds.Sum();
+    }
+}
---
+++ Advent2025/Day02/Tests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2025.Day02;
+
+class Tests
+{
+    [TestCase(example, 1227775554)]
+    public void Test1(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase(example2, 4174379265)]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    public const string example = @"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+    public const string example2 = example;
+}
