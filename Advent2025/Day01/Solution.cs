f470327158b163ee129f7222e40a637591132035
Geerten Vink <maedaros@yahoo.com>
2025-12-02 09:31:31 +0100

    Day 1

---
+++ Advent2025/Day01/Solution.cs
@@ -0,0 +1,68 @@
+﻿namespace Advent2025.Day01;
+
+public class Solution
+{
+    public List<int> rotations = new();
+
+    public Solution(string input)
+    {
+        var lines = Input.GetInputLines(input).ToArray();
+
+        foreach (var line in lines)
+        {
+            if (line.StartsWith("L"))
+                rotations.Add(-int.Parse(line[1..]));
+            else if (line.StartsWith("R"))
+                rotations.Add(int.Parse(line[1..]));
+        }
+    }
+    public Solution() : this("Input.txt") { }
+
+    public int timesAtZero = 0;
+    public int timesPassingZero = 0;
+
+    public void Simulate()
+    {
+        timesAtZero = 0;
+        timesPassingZero = 0;
+
+        int currentRotation = 50;
+
+        for (int n = 0; n < rotations.Count; n++)
+        {
+            var atZero = currentRotation % 100 == 0;
+            var previousFace = GetFace(currentRotation);
+
+            timesPassingZero += Math.Abs(rotations[n]) / 100;
+            currentRotation += rotations[n] % 100;
+
+            var face = GetFace(currentRotation);
+
+            if (face != previousFace && !atZero)
+            {
+                timesPassingZero++;
+            }
+            else if (currentRotation % 100 == 0)
+                timesPassingZero++;
+
+            if (atZero)
+                timesAtZero++;
+        }
+    }
+
+    public static int GetFace(int rotation) => rotation / 100 + (rotation < 0 ? -1 : 0);
+
+    public object GetResult1()
+    {
+        Simulate();
+
+        return timesAtZero;
+    }
+
+    public object GetResult2()
+    {
+        Simulate();
+
+        return timesPassingZero;
+    }
+}
---
+++ Advent2025/Day01/Tests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2025.Day01;
+
+class Tests
+{
+    [TestCase(example, 3)]
+    public void Test1(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase(example2, 6)]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    [TestCase(-168, -2)]
+    [TestCase(-1, -1)]
+    [TestCase(0, 0)]
+    [TestCase(10, 0)]
+    [TestCase(168, 1)]
+    public void CanGetCorrectFace(int rotation, int face)
+    {
+        Solution.GetFace(rotation).Should().Be(face);
+    }
+
+    public const string example = @"L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+
+    public const string example2 = example;
+}
