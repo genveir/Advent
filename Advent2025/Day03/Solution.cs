08b53b759c8fdb9d7cb06a430301300175897bc6
Geerten Vink <maedaros@yahoo.com>
2025-12-03 17:07:29 +0100

    Day 3

---
+++ Advent2025/Day03/Solution.cs
@@ -0,0 +1,64 @@
+﻿namespace Advent2025.Day03;
+
+public class Solution
+{
+    public long[][] grid;
+
+    public Solution(string input)
+    {
+        grid = Input.GetDigitGrid(input).ToArray();
+    }
+    public Solution() : this("Input.txt") { }
+
+    private long getMaxNumber(int row, int digits)
+    {
+        var batteries = new long[digits];
+
+        for (int col = 0; col < grid[row].Length; col++)
+        {
+            var value = grid[row][col];
+
+            int startBattery = Math.Max(0, digits - (grid[row].Length - col));
+
+            for (int battery = startBattery; battery < digits; battery++)
+            {
+                if (value > batteries[battery])
+                {
+                    batteries[battery] = value;
+                    for (int reset = battery + 1; reset < digits; reset++)
+                    {
+                        batteries[reset] = 0;
+                    }
+                    break;
+                }
+            }
+        }
+
+        var stringValue = batteries.Select(n => n.ToString()).Aggregate((a, b) => a + b);
+        return long.Parse(stringValue);
+    }
+
+    public object GetResult1()
+    {
+        List<long> maxNumbers = [];
+
+        for (int n = 0; n < grid.Length; n++)
+        {
+            maxNumbers.Add(getMaxNumber(n, 2));
+        }
+
+        return maxNumbers.Sum();
+    }
+
+    public object GetResult2()
+    {
+        List<long> maxNumbers = [];
+
+        for (int n = 0; n < grid.Length; n++)
+        {
+            maxNumbers.Add(getMaxNumber(n, 12));
+        }
+
+        return maxNumbers.Sum();
+    }
+}
---
+++ Advent2025/Day03/Tests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Advent2025.Day03;
+
+class Tests
+{
+    [TestCase(example, 357)]
+    public void Test1(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult1().Should().Be(output);
+    }
+
+    [TestCase(example2, 3121910778619)]
+    public void Test2(string input, object output)
+    {
+        var sol = new Solution(input);
+
+        sol.GetResult2().Should().Be(output);
+    }
+
+    public const string example = @"987654321111111
+811111111111119
+234234234234278
+818181911112111";
+
+    public const string example2 = example;
+}
